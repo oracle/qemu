62662d6d3e91b46366ce20dc06f2f0d76597f1a8
John Johnson <john.g.johnson@oracle.com>
2020-09-05 18:55:36 -0700

    first commit of VFIO client changes

---
+++ hw/vfio/user.c
@@ -0,0 +1,643 @@
+
+
+/*
+ * vfio protocol over a UNIX socket.
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ *
+ */
+
+#include "qemu/osdep.h"
+#include <linux/vfio.h>
+#include <sys/ioctl.h>
+
+#include "hw/hw.h"
+#include "hw/pci/msi.h"
+#include "hw/pci/msix.h"
+#include "hw/pci/pci_bridge.h"
+#include "hw/qdev-properties.h"
+#include "migration/vmstate.h"
+#include "qemu/error-report.h"
+#include "qemu/main-loop.h"
+#include "qemu/module.h"
+#include "qemu/option.h"
+#include "qemu/range.h"
+#include "qemu/sockets.h"
+#include "qemu/units.h"
+#include "sysemu/kvm.h"
+#include "sysemu/runstate.h"
+#include "sysemu/sysemu.h"
+#include "pci.h"
+#include "user.h"
+#include "trace.h"
+#include "qapi/error.h"
+#include "migration/blocker.h"
+
+
+static void vfio_user_reply_msg(vfio_user_hdr_t *hdr, uint32_t size, uint32_t error);
+static void vfio_user_request_msg(vfio_user_hdr_t *hdr, uint16_t cmd, uint32_t size,
+                                  uint32_t flags);
+
+static void vfio_user_dma_read(VFIOPCIDevice *vdev, struct vfio_user_dma_rw *msg);
+static void vfio_user_dma_write(VFIOPCIDevice *vdev, struct vfio_user_dma_rw *msg);
+static void vfio_user_vm_intr(VFIOPCIDevice *vdev, struct vfio_user_vm_intr *msg);
+static void vfio_user_process_req(VFIOPCIDevice *vdev, char *buf);
+
+static void vfio_user_recv(void *opaque);
+static void vfio_user_send_locked(VFIOProxy *proxy, vfio_user_hdr_t *msg, VFIOUserFDs *fds);
+static void vfio_user_send(VFIOProxy *proxy, vfio_user_hdr_t *msg, VFIOUserFDs *fds);
+static void vfio_user_send_recv(VFIOProxy *proxy, vfio_user_hdr_t *msg, VFIOUserFDs *fds,
+                               int rsize);
+
+VFIOProxy *vfio_user_connect_dev(VFIOPCIDevice *vdev, char *sockname, Error **errp);
+void vfio_user_disconnect(VFIOProxy *proxy);
+
+static void vfio_user_request_msg(vfio_user_hdr_t *hdr, uint16_t cmd, uint32_t size,
+                                  uint32_t flags)
+{
+    static uint16_t next_id = 0;
+
+    hdr->id = next_id++;
+    hdr->command = cmd;
+    hdr->size = size;
+    hdr->flags = (flags & ~VFIO_USER_TYPE) | VFIO_USER_REQUEST;
+    hdr->error_reply = 0;
+}
+
+static void vfio_user_reply_msg(vfio_user_hdr_t *hdr, uint32_t size, uint32_t error)
+{
+    /* convert header to associated reply */
+    hdr->flags = VFIO_USER_REPLY;
+    if (size) {
+        hdr->size = size;
+    }
+    if (error) {
+        hdr->flags |= VFIO_USER_ERROR;
+        hdr->error_reply = error;
+    }
+}
+
+static void vfio_user_dma_read(VFIOPCIDevice *vdev, struct vfio_user_dma_rw *msg)
+{
+    PCIDevice *pdev = &vdev->pdev;
+    vfio_user_hdr_t *hdr;
+    char *buf;
+    int size = msg->count + sizeof(struct vfio_user_dma_rw);
+
+    buf = g_malloc0(size);
+    memcpy(buf, msg, sizeof(*msg));
+
+    pci_dma_read(pdev, msg->offset, buf + sizeof(*msg), msg->count);
+
+    hdr = (vfio_user_hdr_t *)buf;
+    vfio_user_reply_msg(hdr, size, 0);
+    vfio_user_send(vdev->vbasedev.proxy, hdr, NULL);
+
+    g_free(buf);
+}
+
+static void vfio_user_dma_write(VFIOPCIDevice *vdev, struct vfio_user_dma_rw *msg)
+{
+    PCIDevice *pdev = &vdev->pdev;
+    char *buf = (char *)msg + sizeof(*msg);
+
+    pci_dma_write(pdev, msg->offset, buf, msg->count);
+
+    vfio_user_reply_msg(&msg->hdr, sizeof (*msg), 0);
+    vfio_user_send(vdev->vbasedev.proxy, &msg->hdr, NULL);
+}
+
+static void vfio_user_vm_intr(VFIOPCIDevice *vdev, struct vfio_user_vm_intr *msg)
+{
+
+    /* TODO */
+    switch (msg->index) {
+    case VFIO_PCI_INTX_IRQ_INDEX:
+    case VFIO_PCI_MSI_IRQ_INDEX:
+    case VFIO_PCI_MSIX_IRQ_INDEX:
+    default:
+        break;
+    }
+}
+
+static void vfio_user_process_req(VFIOPCIDevice *vdev, char *buf)
+{
+    vfio_user_hdr_t *hdr = (vfio_user_hdr_t *)buf;
+
+    switch (hdr->command) {
+    case VFIO_USER_DMA_READ:
+        vfio_user_dma_read(vdev, (struct vfio_user_dma_rw *)hdr);
+        break;
+    case VFIO_USER_DMA_WRITE:
+        vfio_user_dma_write(vdev, (struct vfio_user_dma_rw *)hdr);
+        break;
+    case VFIO_USER_VM_INTERRUPT:
+        vfio_user_vm_intr(vdev, (struct vfio_user_vm_intr *)hdr);
+        break;
+    default:
+        error_printf("vfio_user_process_req unknown cmd %d\n", hdr->command);
+    }
+}
+
+static void vfio_user_recv(void *opaque)
+{
+    VFIOPCIDevice *vdev = opaque;
+    VFIOProxy *proxy = vdev->vbasedev.proxy;
+    VFIOUserReply *reply = NULL;
+    VFIOUserFDs *fds = NULL;
+    VFIOUserFDs reqfds = { 0, 0, NULL };
+    vfio_user_hdr_t msg;
+    union {
+        char control[CMSG_SPACE(REMOTE_MAX_FDS * sizeof(int))];
+        struct cmsghdr align;
+    } u;
+    struct msghdr hdr;
+    struct cmsghdr *chdr;
+    struct iovec iov = {
+        .iov_base = &msg,
+        .iov_len = sizeof(msg),
+    };
+    int isreply, i, ret, numfds = 0;
+    size_t fdsize, msgleft;
+    int *fdp;
+    char *data, *buf = NULL;
+
+    memset(&hdr, 0, sizeof(hdr));
+    memset(&u, 0, sizeof(u));
+
+    hdr.msg_iov = &iov;
+    hdr.msg_iovlen = 1;
+    hdr.msg_control = &u;
+    hdr.msg_controllen = sizeof(u);
+
+    /*
+     * Since we use a per-proxy lock, let
+     * the iothreads run free
+     */
+    qemu_mutex_unlock_iothread();
+    qemu_mutex_lock(&proxy->lock);
+
+    /*
+     * read the header into 'msg' and read any
+     * control messages into 'cmsg'
+     */
+    ret = recvmsg(proxy->socket, &hdr, 0);
+    if (ret < 0) {
+        error_printf("vfio_user_recv read error\n");
+        goto err;
+    }
+
+    /*
+     * For replies, find the matching pending request
+     */
+    isreply = (msg.flags & VFIO_USER_TYPE) == VFIO_USER_REPLY;
+    if (isreply) {
+         QTAILQ_FOREACH(reply, &proxy->pending, next) {
+            if (msg.id == reply->id) {
+                break;
+            }
+        }
+        if (reply == NULL) {
+            error_printf("vfio_usr_recv unexpected reply\n");
+            goto err;
+        }
+        QTAILQ_REMOVE(&proxy->pending, reply, next);
+        fds = reply->fds;
+    } else {
+        /* 
+         * Yhe client doesn't expect any FDs in requests, so
+         * reqfds.maxfds is 0 here to catch this below
+         * they will be expected on the server, however
+         */
+        fds = &reqfds;
+    }
+
+    /*
+     * Find any embedded file descriptors
+     */
+    for (chdr = CMSG_FIRSTHDR(&hdr); chdr != NULL;
+         chdr = CMSG_NXTHDR(&hdr, chdr)) {
+        if ((chdr->cmsg_level == SOL_SOCKET) &&
+            (chdr->cmsg_type == SCM_RIGHTS)) {
+
+            fdp = (int *)CMSG_DATA(chdr);
+            fdsize = chdr->cmsg_len - CMSG_LEN(0);
+            numfds = fdsize / sizeof(int);
+
+            if (fds == NULL || fds->maxfds < numfds) {
+                error_printf("vfio_user_recv unexpected FDs\n");
+                goto err;
+            }
+            fds->numfds = numfds;
+            memcpy(fds->fds, fdp, fdsize);
+            break;
+        }
+    }
+
+    /*
+     * put the whole message into a single buffer
+     */
+    msgleft = msg.size - sizeof(msg);
+    if (isreply) {
+        if (msg.size > reply->rsize) {
+            error_printf("reply larger than recv buffer\n");
+            goto err;
+        }
+        *reply->msg = msg;
+        data = (char *)reply->msg + sizeof(msg);
+    } else {
+        buf = g_malloc0(msg.size);
+        memcpy(buf, &msg, sizeof(msg));
+        data = buf + sizeof(msg);
+    }
+
+    ret = read(proxy->socket, data, msgleft);
+    if (ret != msgleft) {
+        error_printf("short read of msg body\n");
+        goto err;
+    }
+    qemu_mutex_unlock(&proxy->lock);
+
+    /*
+     * Replies signal a waiter, requests get processed by vfio code
+     * that may assume the iothread lock is held.
+     * Either way, make sure the iothread is locked before we return.
+     */
+    if (isreply) {
+        reply->complete = 1;
+        qemu_cond_signal(&reply->cv);
+        qemu_mutex_lock_iothread();
+    } else {
+        qemu_mutex_lock_iothread();
+        vfio_user_process_req(vdev, buf);
+    }
+    return;
+
+err:
+    qemu_mutex_unlock(&proxy->lock);
+    for (i = 0; i < numfds; i++) {
+        close(fdp[i]);
+    }
+    if (buf != NULL) {
+        g_free(buf);
+    }
+    if (reply != NULL) {
+        /* force an error to keep sending thread from hanging */ 
+        reply->msg->flags |= VFIO_USER_ERROR;
+        reply->msg->error_reply = EINVAL;
+        reply->complete = 1;
+        qemu_cond_signal(&reply->cv);
+    }
+    qemu_mutex_lock_iothread();
+}
+    
+            
+static void vfio_user_send_locked(VFIOProxy *proxy, vfio_user_hdr_t *msg, VFIOUserFDs *fds)
+{
+    union {
+        char control[CMSG_SPACE(REMOTE_MAX_FDS * sizeof(int))];
+        struct cmsghdr align;
+    } u;
+    struct msghdr hdr;
+    struct cmsghdr *chdr;
+    struct iovec iov = {
+        .iov_base = msg,
+        .iov_len = msg->size,
+    };
+    int ret;
+
+    memset(&hdr, 0, sizeof(hdr));
+    hdr.msg_iov = &iov;
+    hdr.msg_iovlen = 1;
+
+    if (fds != NULL && fds->numfds > 0) {
+        size_t fdsize = fds->numfds * sizeof(int);
+
+        memset(&u, 0, sizeof(u));
+        hdr.msg_control = &u;
+        hdr.msg_controllen = sizeof(u);
+    
+        chdr = CMSG_FIRSTHDR(&hdr);
+        chdr->cmsg_len = CMSG_LEN(fdsize);
+        chdr->cmsg_level = SOL_SOCKET;
+        chdr->cmsg_type = SCM_RIGHTS;
+        memcpy(CMSG_DATA(chdr), fds->fds, fdsize);
+        hdr.msg_controllen = CMSG_SPACE(fdsize);
+    }
+
+    do {
+        ret = sendmsg(proxy->socket, &hdr, 0);
+    } while (ret < 0 && errno == EINTR);
+
+    if (ret < 0) {
+        error_printf("vfio_user_send errno %d\n", errno);
+    }
+}
+
+static void vfio_user_send(VFIOProxy *proxy, vfio_user_hdr_t *msg, VFIOUserFDs *fds)
+{
+    /* Use the same locking protocol as vfio_user_send_recv() below */
+    qemu_mutex_unlock_iothread();
+    qemu_mutex_lock(&proxy->lock);
+    vfio_user_send_locked(proxy, msg, fds);
+    qemu_mutex_unlock(&proxy->lock);
+    qemu_mutex_lock_iothread();
+}
+
+static void vfio_user_send_recv(VFIOProxy *proxy, vfio_user_hdr_t *msg, VFIOUserFDs *fds,
+                               int rsize)
+{
+    VFIOUserReply *reply;
+
+    if (msg->flags & VFIO_USER_NO_REPLY) {
+        error_printf("vfio_user_send_recv on async message\n");
+        return;
+    }
+
+    /*
+     * We will block later, so use a per-proxy lock and let
+     * the iothreads run while we sleep.
+     */
+    qemu_mutex_unlock_iothread();
+    qemu_mutex_lock(&proxy->lock);
+
+    reply = QTAILQ_FIRST(&proxy->free);
+    if (reply != NULL) {
+        QTAILQ_REMOVE(&proxy->free, reply, next);
+        reply->complete = 0;
+    } else {
+        reply = g_malloc0(sizeof(*reply));
+        qemu_cond_init(&reply->cv);
+    }
+    reply->msg = msg;
+    reply->fds = fds;
+    reply->id = msg->id;
+    reply->rsize = rsize ? rsize : msg->size;
+    QTAILQ_INSERT_TAIL(&proxy->pending, reply, next);
+
+    vfio_user_send_locked(proxy, msg, fds);
+    while (reply->complete == 0) {
+        qemu_cond_wait(&reply->cv, &proxy->lock);
+    }
+
+    QTAILQ_INSERT_HEAD(&proxy->free, reply, next);
+    qemu_mutex_unlock(&proxy->lock);
+    qemu_mutex_lock_iothread();
+}
+
+VFIOProxy *vfio_user_connect_dev(VFIOPCIDevice *vdev, char *sockname, Error **errp)
+{
+    VFIOProxy *proxy;
+    int sockfd;
+
+    sockfd = unix_connect(sockname, errp);
+    if (sockfd == -1) {
+        return NULL;
+    }
+
+    proxy = g_malloc0(sizeof(VFIOProxy));
+    proxy->socket = sockfd;
+    proxy->sockname = sockname;
+
+    qemu_mutex_init(&proxy->lock);
+    QTAILQ_INIT(&proxy->free);
+    QTAILQ_INIT(&proxy->pending);
+    qemu_set_fd_handler(proxy->socket, vfio_user_recv, NULL, vdev);
+
+    return proxy;
+}
+
+void vfio_user_disconnect(VFIOProxy *proxy)
+{
+    VFIOUserReply *r1, *r2;
+
+    qemu_set_fd_handler(proxy->socket, NULL, NULL, NULL);
+    QTAILQ_FOREACH_SAFE(r1, &proxy->free, next, r2) {
+        QTAILQ_REMOVE(&proxy->free, r1, next);
+        g_free(r1);
+    }
+    close(proxy->socket);
+    g_free(proxy);
+}
+
+int vfio_user_validate_version(VFIODevice *vbasedev, Error **errp)
+{
+    struct vfio_user_version msg;
+
+    vfio_user_request_msg(&msg.hdr, VFIO_USER_VERSION, sizeof(msg), 0);
+    msg.major = 1;
+    msg.minor = 0;
+
+    vfio_user_send_recv(vbasedev->proxy, &msg.hdr, NULL, 0);
+    if (msg.hdr.flags & VFIO_USER_ERROR) {
+        error_setg_errno(errp, msg.hdr.error_reply, "version reply");
+        return -1;
+    }
+
+    if (msg.major != VFIO_USER_MAJOR_VER || msg.minor > VFIO_USER_MINOR_VER) {
+        error_setg(errp, "incompatible server version");
+        return -1;
+    }
+    return 0;
+}
+
+int vfio_user_dma_map(VFIODevice *vbasedev, struct vfio_user_map *map, VFIOUserFDs *fds,
+                      uint64_t nelem)
+{
+    struct vfio_user_dma_map *msgp;
+    int size = sizeof(*msgp) + (nelem * sizeof(struct vfio_user_map));
+    int ret;
+
+    if (fds->numfds != nelem) {
+        error_printf("vfio_user_dma_map mismatch of FDs and table elements\n");
+        return -1;
+    }
+    msgp = g_malloc0(size);
+    vfio_user_request_msg(&msgp->hdr, VFIO_USER_DMA_MAP, size, 0);
+    memcpy(&msgp->table, map, nelem * sizeof(*map));
+
+    vfio_user_send_recv(vbasedev->proxy, &msgp->hdr, fds, 0);
+    ret = (msgp->hdr.flags & VFIO_USER_ERROR) ? -1 : 0;
+
+    g_free(msgp);
+    return ret;
+}
+
+int vfio_user_dma_unmap(VFIODevice *vbasedev, struct vfio_user_map *map,
+                               uint64_t nelem)
+{
+    struct vfio_user_dma_map *msgp;
+    int size = sizeof(*msgp) + (nelem * sizeof(struct vfio_user_map));
+    int ret;
+
+    msgp = g_malloc0(size);
+    vfio_user_request_msg(&msgp->hdr, VFIO_USER_DMA_UNMAP, size, 0);
+    memcpy(&msgp->table, map, nelem * sizeof(*map));
+
+    vfio_user_send_recv(vbasedev->proxy, &msgp->hdr, NULL, 0);
+    ret = (msgp->hdr.flags & VFIO_USER_ERROR) ? -1 : 0;
+
+    g_free(msgp);
+    return ret;
+}
+
+int vfio_user_get_info(VFIODevice *vbasedev)
+{
+    struct vfio_user_device_info msg;
+    struct vfio_device_info *info;
+
+    memset(&msg, 0, sizeof(msg));
+    vfio_user_request_msg(&msg.hdr, VFIO_USER_DEVICE_GET_INFO, sizeof(msg), 0);
+
+    vfio_user_send_recv(vbasedev->proxy, &msg.hdr, NULL, 0);
+    if (msg.hdr.flags & VFIO_USER_ERROR) {
+        return -msg.hdr.error_reply;
+    }
+
+    info = &msg.dev_info;
+    vbasedev->num_irqs = info->num_irqs;
+    vbasedev->num_regions = info->num_regions;
+    vbasedev->flags = info->flags;
+    vbasedev->reset_works = !!(info->flags & VFIO_DEVICE_FLAGS_RESET);
+    return 0;
+}
+
+int vfio_user_get_region_info(VFIODevice *vbasedev, int index,
+                                     struct vfio_region_info *info, VFIOUserFDs *fds)
+{
+    struct vfio_user_region_info *msgp;
+    int size;
+
+    /* data returned can be larger than vfio_region_info */
+    if (info->argsz < sizeof(*info)) {
+        error_printf("vfio_user_get_region_info argsz too small\n");
+        return -1;
+    }
+    size = info->argsz + sizeof(vfio_user_hdr_t);
+    msgp = g_malloc0(size);
+
+    vfio_user_request_msg(&msgp->hdr, VFIO_USER_DEVICE_GET_REGION_INFO, sizeof(*msgp), 0);
+    msgp->reg_info = *info;
+
+    vfio_user_send_recv(vbasedev->proxy, &msgp->hdr, fds, size);
+    if (msgp->hdr.flags & VFIO_USER_ERROR) {
+        return -msgp->hdr.error_reply;
+    }
+
+    memcpy(info, &msgp->reg_info, info->argsz);
+    return 0;
+}
+
+int vfio_user_get_irq_info(VFIODevice *vbasedev, struct vfio_irq_info *info)
+{
+    struct vfio_user_irq_info msg;
+
+    memset(&msg, 0, sizeof(msg));
+    vfio_user_request_msg(&msg.hdr, VFIO_USER_DEVICE_GET_IRQ_INFO, sizeof(msg), 0);
+
+    vfio_user_send_recv(vbasedev->proxy, &msg.hdr, NULL, 0);
+    if (msg.hdr.flags & VFIO_USER_ERROR) {
+        return -msg.hdr.error_reply;
+    }
+
+    *info = msg.irq_info;
+    return 0;
+}
+
+int vfio_user_set_irq_info(VFIODevice *vbasedev, struct vfio_irq_set *irq)
+{
+    struct vfio_user_irq_set smallmsg, *msgp;
+
+    /* only allocate if sending data array with set message */
+    if (irq->argsz < sizeof(*irq)) {
+        error_printf("vfio_user_set_irq_info argsz too small\n");
+        return -1;
+    }
+    msgp = irq->argsz > sizeof(smallmsg) ? g_malloc0(irq->argsz) : &smallmsg;
+
+    vfio_user_request_msg(&msgp->hdr, VFIO_USER_DEVICE_GET_IRQ_INFO, irq->argsz, 0);
+    memcpy(&msgp->irq_set, irq, irq->argsz);
+
+    vfio_user_send_recv(vbasedev->proxy, &msgp->hdr, NULL, 0);
+    if (msgp->hdr.flags & VFIO_USER_ERROR) {
+        return -msgp->hdr.error_reply;
+    }
+
+    if (msgp != &smallmsg) {
+        g_free(msgp);
+    }
+    return 0;
+}
+
+int vfio_user_region_read(VFIODevice *vbasedev, uint32_t index, uint64_t offset,
+                                 uint32_t count, void *data)
+{
+    struct {
+        struct vfio_user_region_rw msg;
+        char buf[8];
+    } smallmsg;
+    struct vfio_user_region_rw *msgp;
+    int size = sizeof(*msgp) + count;
+
+    /* most reads are just registers, only allocate for larger ones */
+    msgp = count > 8 ? g_malloc0(size) : &smallmsg.msg;
+    vfio_user_request_msg(&msgp->hdr, VFIO_USER_REGION_READ, sizeof(*msgp), 0);
+    msgp->offset = offset;
+    msgp->region = index;
+    msgp->count = count;
+
+    vfio_user_send_recv(vbasedev->proxy, &msgp->hdr, NULL, size);
+    if (msgp->hdr.flags & VFIO_USER_ERROR) {
+        errno = msgp->hdr.error_reply;
+    } else if (msgp->count > count) {
+        errno = E2BIG;
+    } else {
+        errno = 0;
+        memcpy(data, (char *)msgp + sizeof(*msgp), msgp->count);
+    }
+
+    if (msgp != &smallmsg.msg) {
+        g_free(msgp);
+    }
+    return errno ? -1: msgp->count;
+}
+
+int vfio_user_region_write(VFIODevice *vbasedev, uint32_t index, uint64_t offset,
+                                  uint32_t count, void *data)
+{
+     struct {
+        struct vfio_user_region_rw msg;
+        char buf[8];
+    } smallmsg;
+    struct vfio_user_region_rw *msgp;
+    int size = sizeof(*msgp) + count;
+
+    /* most writes are just registers, only allocate for larger ones */
+    msgp = count > 8 ? g_malloc0(size) : &smallmsg.msg;
+    vfio_user_request_msg(&msgp->hdr, VFIO_USER_REGION_WRITE, size, VFIO_USER_NO_REPLY);
+    msgp->offset = offset;
+    msgp->region = index;
+    msgp->count = count;
+    memcpy((char *)msgp + sizeof(*msgp), data, count);
+
+    vfio_user_send(vbasedev->proxy, &msgp->hdr, NULL);
+
+    if (msgp != &smallmsg.msg) {
+        g_free(msgp);
+    }
+    return count;
+}
+
+void vfio_user_reset(DeviceState *dev)
+{
+    VFIOPCIDevice *vdev = PCI_VFIOU(dev);
+    vfio_user_hdr_t msg;
+    
+    vfio_user_request_msg(&msg, VFIO_USER_DEVICE_RESET, sizeof(msg), 0);
+
+    vfio_user_send_recv(vdev->vbasedev.proxy, &msg, NULL, 0);
+    if (msg.flags & VFIO_USER_ERROR) {
+        error_printf("reset reply error %d\n", msg.error_reply);
+    }
+}
---
+++ hw/vfio/user.h
@@ -0,0 +1,191 @@
+
+
+
+/*
+ * vfio protocol over a UNIX socket.
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ *
+ *
+ * Each message has a standard header that describes the command
+ * being sent, which is almost always a VFIO ioctl().
+ *
+ * The header may be followed by command-specfic data, such as the
+ * region and offset info for read and write commands.
+ */
+
+typedef struct vfio_user_hdr {
+    uint16_t id;
+    uint16_t command;
+    uint32_t size;
+    uint32_t flags;
+    uint32_t error_reply;
+} vfio_user_hdr_t;
+
+/* commands */
+#define VFIO_USER_VERSION			1
+#define VFIO_USER_DMA_MAP			2
+#define VFIO_USER_DMA_UNMAP			3
+#define VFIO_USER_DEVICE_GET_INFO		4
+#define VFIO_USER_DEVICE_GET_REGION_INFO	5
+#define VFIO_USER_DEVICE_GET_IRQ_INFO		6
+#define VFIO_USER_DEVICE_SET_IRQS		7
+#define VFIO_USER_REGION_READ			8
+#define VFIO_USER_REGION_WRITE			9
+#define VFIO_USER_DMA_READ			10
+#define VFIO_USER_DMA_WRITE			11
+#define VFIO_USER_VM_INTERRUPT			12
+#define VFIO_USER_DEVICE_RESET			13
+
+
+/* flags */
+#define VFIO_USER_REQUEST	0x0
+#define VFIO_USER_REPLY		0x1
+#define VFIO_USER_TYPE		0xF
+
+#define VFIO_USER_NO_REPLY	0x10
+#define VFIO_USER_ERROR		0x20
+
+
+
+/*
+ * VFIO_USER_VERSION
+ */
+struct vfio_user_version {
+    vfio_user_hdr_t hdr;
+    uint16_t major;
+    uint16_t minor;
+};
+
+#define VFIO_USER_MAJOR_VER	1
+#define VFIO_USER_MINOR_VER	0
+
+/*
+ * VFIO_USER_DMA_MAP
+ * VFIO_USER_DMA_UNMAP
+ */
+struct vfio_user_map {
+    uint64_t address;
+    uint64_t size;
+    uint64_t offset;
+    uint32_t protection;
+    uint32_t flags;
+};
+
+struct vfio_user_dma_map {
+    vfio_user_hdr_t hdr;
+    struct vfio_user_map table[];
+};
+
+#define VFIO_USER_MAPPABLE	1
+
+/*
+ * VFIO_USER_DEVICE_GET_INFO
+ */
+struct vfio_user_device_info {
+    vfio_user_hdr_t hdr;
+    struct vfio_device_info dev_info;
+};
+
+/*
+ * VFIO_USER_DEVICE_GET_REGION_INFO
+ */
+struct vfio_user_region_info {
+    vfio_user_hdr_t hdr;
+    struct vfio_region_info reg_info;
+};
+
+/*
+ * VFIO_USER_DEVICE_GET_IRQ_INFO
+ */
+struct vfio_user_irq_info {
+    vfio_user_hdr_t hdr;
+    struct vfio_irq_info irq_info;
+};
+
+/*
+ * VFIO_USER_DEVICE_SET_IRQS
+ */
+struct vfio_user_irq_set {
+    vfio_user_hdr_t hdr;
+    struct vfio_irq_set irq_set;
+};
+
+
+/*
+ * VFIO_USER_REGION_READ
+ * VFIO_USER_REGION_WRITE
+ */
+struct vfio_user_region_rw {
+    vfio_user_hdr_t hdr;
+    uint64_t offset;
+    uint32_t region;
+    uint32_t count;
+    /* data */
+};
+
+/*
+ * VFIO_USER_DMA_READ
+ * VFIO_USER_DMA_WRITE
+ */
+struct vfio_user_dma_rw {
+    vfio_user_hdr_t hdr;
+    uint64_t offset;
+    uint32_t count;
+    /* data */
+};
+
+/*
+ * VFIO_USER_VM_INTERRUPT
+ */
+struct vfio_user_vm_intr {
+    vfio_user_hdr_t hdr;
+    uint32_t index;
+    uint32_t subindex;
+};
+
+#define REMOTE_MAX_FDS	10
+
+typedef struct VFIOUserFDs {
+    int numfds;
+    int maxfds;
+    int *fds;
+} VFIOUserFDs;
+
+typedef struct VFIOUserReply {
+    QTAILQ_ENTRY(VFIOUserReply) next;
+    vfio_user_hdr_t *msg;
+    VFIOUserFDs *fds;
+    int rsize;
+    uint32_t id;
+    QemuCond cv;
+    uint8_t complete;
+} VFIOUserReply;
+
+typedef struct VFIOProxy {
+    QTAILQ_HEAD(, VFIOUserReply) free;
+    QTAILQ_HEAD(, VFIOUserReply) pending;
+    int socket;
+    char *sockname;
+    QemuMutex lock;
+} VFIOProxy;
+
+
+#define TYPE_VFIO_USER_PCI "vfio-user-pci"
+#define PCI_VFIOU(obj)    OBJECT_CHECK(VFIOPCIDevice, obj, TYPE_VFIO_USER_PCI)
+
+int vfio_user_validate_version(VFIODevice *vbasedev, Error **errp);
+int vfio_user_dma_map(VFIODevice *vbasedev, struct vfio_user_map *map, VFIOUserFDs *fds,
+                      uint64_t nelem);
+int vfio_user_dma_unmap(VFIODevice *vbasedev, struct vfio_user_map *map, uint64_t nelem);
+int vfio_user_get_info(VFIODevice *vbasedev);
+int vfio_user_get_region_info(VFIODevice *vbasedev, int index, struct vfio_region_info *info,
+                              VFIOUserFDs *fds);
+int vfio_user_get_irq_info(VFIODevice *vbasedev, struct vfio_irq_info *info);
+int vfio_user_set_irq_info(VFIODevice *vbasedev, struct vfio_irq_set *irq);
+int vfio_user_region_read(VFIODevice *vbasedev, uint32_t index, uint64_t offset,
+                          uint32_t count, void *data);
+int vfio_user_region_write(VFIODevice *vbasedev, uint32_t index, uint64_t offset,
+                           uint32_t count, void *data);
+void vfio_user_reset(DeviceState *dev);
