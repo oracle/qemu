8bc3133469b42eecc3a8af8200b5ce9692e6f3e9
John Johnson <john.g.johnson@oracle.com>
2021-08-07 16:57:23 -0700

    vfio-user: get device info
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Signed-off-by: John G Johnson <john.g.johnson@oracle.com>
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -254,6 +254,9 @@ void vfio_put_group(VFIOGroup *group);
 int vfio_get_device(VFIOGroup *group, const char *name,
                     VFIODevice *vbasedev, Error **errp);
 
+void vfio_init_device(VFIODevice *vbasedev, VFIOGroup *group,
+                      struct vfio_device_info *info);
+
 extern const MemoryRegionOps vfio_region_ops;
 typedef QLIST_HEAD(VFIOGroupList, VFIOGroup) VFIOGroupList;
 extern VFIOGroupList vfio_group_list;
