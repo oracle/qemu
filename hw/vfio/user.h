12b4b10df3ee9c8984927f23aec5ca5a8c5fbd2b
Jagannathan Raman <jag.raman@oracle.com>
2021-08-08 18:35:54 -0700

    vfio-user: region read/write
    Add support for posted writes on remote devices
    
    Originally-by: John Johnson <john.g.johnson@oracle.com>
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>

--- hw/vfio/user.h
+++ hw/vfio/user.h
@@ -84,6 +84,7 @@ typedef struct VFIOUserProxy {
 /* VFIOProxy flags */
 #define VFIO_PROXY_CLIENT        0x1
 #define VFIO_PROXY_FORCE_QUEUED  0x4
+#define VFIO_PROXY_NO_POST       0x8
 
 VFIOUserProxy *vfio_user_connect_dev(SocketAddress *addr, Error **errp);
 void vfio_user_disconnect(VFIOUserProxy *proxy);
