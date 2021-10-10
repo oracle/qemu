8f03eb8e8b1ed0916f5268a99c526ba7a2dc3d4b
John Johnson <john.g.johnson@oracle.com>
2021-10-10 11:23:37 -0700

    vfio-user: define socket send functions
    Also negotiate protocol version with remote server
    
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Signed-off-by: John G Johnson <john.g.johnson@oracle.com>

--- hw/vfio/user.h
+++ hw/vfio/user.h
@@ -54,6 +54,8 @@ typedef struct VFIOProxy {
     struct QIOChannel *ioc;
     void (*request)(void *opaque, VFIOUserMsg *msg);
     void *req_arg;
+    uint64_t max_xfer_size;
+    uint64_t max_send_fds;
     int flags;
     QemuCond close_cv;
     AioContext *ctx;
@@ -76,11 +78,14 @@ typedef struct VFIOProxy {
 
 /* VFIOProxy flags */
 #define VFIO_PROXY_CLIENT        0x1
+#define VFIO_PROXY_FORCE_QUEUED  0x4
+#define VFIO_PROXY_USE_MULTI     0x10
 
 VFIOProxy *vfio_user_connect_dev(SocketAddress *addr, Error **errp);
 void vfio_user_disconnect(VFIOProxy *proxy);
 void vfio_user_set_handler(VFIODevice *vbasedev,
                            void (*handler)(void *opaque, VFIOUserMsg *msg),
                            void *reqarg);
+int vfio_user_validate_version(VFIOProxy *proxy, Error **errp);
 
 #endif /* VFIO_USER_H */
