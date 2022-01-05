9fb5aa387bd04c498e6759a6ec45987f1dbf55d0
John Johnson <john.g.johnson@oracle.com>
2022-01-05 15:56:25 -0800

    vfio-user: define socket receive functions
    Add infrastructure needed to receive incoming messages
    
    Signed-off-by: John G Johnson <john.g.johnson@oracle.com>
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>

--- hw/vfio/user.h
+++ hw/vfio/user.h
@@ -11,6 +11,8 @@
  *
  */
 
+#include "user-protocol.h"
+
 typedef struct {
     int send_fds;
     int recv_fds;
@@ -27,6 +29,7 @@ enum msg_type {
 
 typedef struct VFIOUserMsg {
     QTAILQ_ENTRY(VFIOUserMsg) next;
+    VFIOUserHdr *hdr;
     VFIOUserFDs *fds;
     uint32_t rsize;
     uint32_t id;
@@ -66,6 +69,8 @@ typedef struct VFIOUserProxy {
     VFIOUserMsgQ incoming;
     VFIOUserMsgQ outgoing;
     VFIOUserMsg *last_nowait;
+    VFIOUserMsg *part_recv;
+    size_t recv_left;
     enum proxy_state state;
 } VFIOUserProxy;
 
@@ -74,5 +79,8 @@ typedef struct VFIOUserProxy {
 
 VFIOUserProxy *vfio_user_connect_dev(SocketAddress *addr, Error **errp);
 void vfio_user_disconnect(VFIOUserProxy *proxy);
+void vfio_user_set_handler(VFIODevice *vbasedev,
+                           void (*handler)(void *opaque, VFIOUserMsg *msg),
+                           void *reqarg);
 
 #endif /* VFIO_USER_H */
