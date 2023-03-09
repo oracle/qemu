9de9fdb7b112678867ca64933bce645871ee773b
Wei Wang <wei.w.wang@intel.com>
2023-03-09 18:29:56 +0800

    migration/multifd: correct multifd_send_thread to trace the flags
    The p->flags could be updated via the send_prepare callback, e.g. OR-ed
    with MULTIFD_FLAG_ZLIB via zlib_send_prepare. Assign p->flags to the
    local "flags" before the send_prepare callback could only get partial of
    p->flags. Fix it by moving the assignment of p->flags to the local flags
    after the callback, so that the correct flags can be traced.
    
    Fixes: ab7cbb0b9a3b ("multifd: Make no compression operations into its own structure")
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    (cherry picked from commit ff1585d1d8ae5b15d88327e59859d352829caebb)
    
    Orabug: 35636284
    
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>

--- migration/multifd.c
+++ migration/multifd.c
@@ -673,7 +673,7 @@ static void *multifd_send_thread(void *opaque)
 
         if (p->pending_job) {
             uint64_t packet_num = p->packet_num;
-            uint32_t flags = p->flags;
+            uint32_t flags;
             p->normal_num = 0;
 
             if (use_zero_copy_send) {
@@ -695,6 +695,7 @@ static void *multifd_send_thread(void *opaque)
                 }
             }
             multifd_send_fill_packet(p);
+            flags = p->flags;
             p->flags = 0;
             p->num_packets++;
             p->total_normal_pages += p->normal_num;
