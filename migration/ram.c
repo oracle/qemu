ad43b02bcd6d874f8152928fe1b8a7ae37eea630
Juan Quintela <quintela@redhat.com>
2023-04-11 17:38:11 +0200

    migration: Make downtime_bytes atomic
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    (cherry picked from commit 296a4ac2aa63038b6b702f2ee8f0f93ae26727ae)
    
    Orabug: 35636284
    
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>

--- migration/ram.c
+++ migration/ram.c
@@ -435,7 +435,7 @@ void ram_transferred_add(uint64_t bytes)
     } else if (migration_in_postcopy()) {
         stat64_add(&ram_counters.postcopy_bytes, bytes);
     } else {
-        ram_counters.downtime_bytes += bytes;
+        stat64_add(&ram_counters.downtime_bytes, bytes);
     }
     stat64_add(&ram_counters.transferred, bytes);
 }
