fac2e618276ed248f372a5e60b39314232154395
Juan Quintela <quintela@redhat.com>
2023-05-04 13:38:39 +0200

    qemu-file: Make total_transferred an uint64_t
    Change all the functions that use it.  It was already passed as
    uint64_t.
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20230504113841.23130-8-quintela@redhat.com>
    (cherry picked from commit 61abf1ebdcddafdb62ba6784e04c573ae6beace6)
    
     Conflicts:
    	migration/block.c
            No patch a37174025178 ("migration: qemu_file_total_transferred()
            function is monotonic")
    
    Orabug: 35636284
    
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>

--- migration/qemu-file.c
+++ migration/qemu-file.c
@@ -51,7 +51,7 @@ struct QEMUFile {
     int64_t rate_limit_used;
 
     /* The sum of bytes transferred on the wire */
-    int64_t total_transferred;
+    uint64_t total_transferred;
 
     int buf_index;
     int buf_size; /* 0 when writing */
@@ -722,9 +722,9 @@ int qemu_get_byte(QEMUFile *f)
     return result;
 }
 
-int64_t qemu_file_total_transferred_fast(QEMUFile *f)
+uint64_t qemu_file_total_transferred_fast(QEMUFile *f)
 {
-    int64_t ret = f->total_transferred;
+    uint64_t ret = f->total_transferred;
     int i;
 
     for (i = 0; i < f->iovcnt; i++) {
@@ -734,7 +734,7 @@ int64_t qemu_file_total_transferred_fast(QEMUFile *f)
     return ret;
 }
 
-int64_t qemu_file_total_transferred(QEMUFile *f)
+uint64_t qemu_file_total_transferred(QEMUFile *f)
 {
     qemu_fflush(f);
     return f->total_transferred;
