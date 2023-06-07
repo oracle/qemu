1e8c715231b6141cce12953020f6eff192a4002e
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2023-06-07 15:57:17 -0500

    pc: q35: Bump max_cpus to 1024
    Since KVM_MAX_VCPUS is currently defined to 1024 for x86 as shown in
    arch/x86/include/asm/kvm_host.h, update QEMU limits to the same number.
    
    In case KVM could not support the specified number of vcpus, QEMU would
    return the following error message:
    
      qemu-system-x86_64: kvm_init_vcpu: kvm_get_vcpu failed (xxx): Invalid argument
    
    Also, keep max_cpus at 288 for machine version 8.0 and older.
    
    Cc: Igor Mammedov <imammedo@redhat.com>
    Cc: Daniel P. Berrangé <berrange@redhat.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Cc: Julia Suvorova <jusual@redhat.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Message-Id: <20230607205717.737749-3-suravee.suthikulpanit@amd.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    
    Orabug: 35425619
    
    The cherry-pick was modified such that max_cpus is set to 1024
    for all versions of the q35 machine type, not just for QEMU
    versions 8.1 and later.
    
    (cherry picked from commit e0001297eb2f8569e950e55dbda8ad686e4155fb)
    Signed-off-by: Karl Heubaum <karl.heubaum@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>
    
    Conflicts:
        hw/i386/pc_q35.c - Git contextual confusion, max_cpus=1024 for
        all versions of QEMU

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -363,11 +363,11 @@ static void pc_q35_machine_options(MachineClass *m)
     m->default_display = "std";
     m->default_kernel_irqchip_split = false;
     m->no_floppy = 1;
+    m->max_cpus = 1024;
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_AMD_IOMMU_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_INTEL_IOMMU_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_RAMFB_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_VMBUS_BRIDGE);
-    m->max_cpus = 288;
 }
 
 static void pc_q35_7_2_machine_options(MachineClass *m)
