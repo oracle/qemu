9fe64b183f10d14b979cffa40e5876fc437ca36a
Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
2023-06-23 13:26:25 -0700

    target/i386: Export MSR_ARCH_CAPABILITIES bits to guests
    On Intel CPUs there are certain bits in MSR_ARCH_CAPABILITIES that
    indicates if the CPU is not affected by a vulnerability. Without these
    bits guests may try to deploy the mitigation even if the CPU is not
    affected.
    
    Export the bits to guests that indicate immunity to hardware
    vulnerabilities.
    
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Message-ID: <63d85cc76d4cdc51e6c732478b81d8f13be11e5a.1687551881.git.pawan.kumar.gupta@linux.intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 5bef742cc4f0e21c80a31611af7881ba811e507f)
    
    JIRA: https://jira.oci.oraclecorp.com/browse/LINUX-14435
    
    Conflicts:
    	target/i386/cpu.c
    
    Contextual conflict missing:
    22e1094ca82d ("target/i386: add support for FB_CLEAR feature")
    
    Signed-off-by: Mark Kanda <mark.kanda@oracle.com>
    Reviewed-by: Ross Philipson <ross.philipson@oracle.com>
    Reviewed-by: Karl Heubaum <karl.heubaum@oracle.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -1062,10 +1062,10 @@ FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
             "rdctl-no", "ibrs-all", "rsba", "skip-l1dfl-vmentry",
             "ssb-no", "mds-no", "pschange-mc-no", "tsx-ctrl",
             "taa-no", NULL, NULL, NULL,
+            NULL, "sbdr-ssdp-no", "fbsdp-no", "psdp-no",
             NULL, NULL, NULL, NULL,
             NULL, NULL, NULL, NULL,
-            NULL, NULL, NULL, NULL,
-            NULL, NULL, NULL, NULL,
+            "pbrsb-no", NULL, NULL, NULL,
             NULL, NULL, NULL, NULL,
         },
         .msr = {
