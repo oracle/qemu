e07ab31e354f7cb4d0793c6418f75dcea997c07a
Avihai Horon <avihaih@nvidia.com>
2023-07-11 13:56:59 +0100

    vfio/migration: Allow migration of multiple P2P supporting devices
    Now that P2P support has been added to VFIO migration, allow migration
    of multiple devices if all of them support P2P.
    
    Single device migration is allowed regardless of P2P support.
    
    JIRA: https://jira.oci.oraclecorp.com/browse/LINUX-13969
    Signed-off-by: Avihai Horon <avihaih@nvidia.com>
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Reviewed-by: Karl Heubaum <karl.heubaum@oracle.com>

--- hw/vfio/common.c
+++ hw/vfio/common.c
@@ -363,21 +363,30 @@ bool vfio_mig_active(void)
 
 static Error *multiple_devices_migration_blocker;
 
-static unsigned int vfio_migratable_device_num(void)
+/*
+ * Multiple devices migration is currently allowed only if all devices support P2P.
+ * Single device migration is allowed regardless of P2P support.
+ */
+static unsigned int vfio_should_block_non_p2p_migration(void)
 {
     VFIOGroup *group;
     VFIODevice *vbasedev;
     unsigned int device_num = 0;
+    bool have_non_p2p_device = false;
 
     QLIST_FOREACH(group, &vfio_group_list, next) {
         QLIST_FOREACH(vbasedev, &group->device_list, next) {
             if (vbasedev->migration) {
                 device_num++;
+
+                if (!(vbasedev->migration->mig_flags & VFIO_MIGRATION_P2P)) {
+                    have_non_p2p_device = true;
+                }
             }
         }
     }
 
-    return device_num;
+    return have_non_p2p_device && device_num > 1;
 }
 
 int vfio_block_multiple_devices_migration(VFIODevice *vbasedev, Error **errp)
@@ -385,19 +394,19 @@ int vfio_block_multiple_devices_migration(VFIODevice *vbasedev, Error **errp)
     int ret;
 
     if (multiple_devices_migration_blocker ||
-        vfio_migratable_device_num() <= 1) {
+        !vfio_should_block_non_p2p_migration()) {
         return 0;
     }
 
     if (vbasedev->enable_migration == ON_OFF_AUTO_ON) {
-        error_setg(errp, "Migration is currently not supported with multiple "
-                         "VFIO devices");
+        error_setg(errp, "Migration is currently only supported with multiple "
+                         "VFIO devices when all of them support P2P");
         return -EINVAL;
     }
 
     error_setg(&multiple_devices_migration_blocker,
-               "Migration is currently not supported with multiple "
-               "VFIO devices");
+               "Migration is currently only supported with multiple "
+               "VFIO devices when all of them support P2P");
     ret = migrate_add_blocker(multiple_devices_migration_blocker, errp);
     if (ret < 0) {
         error_free(multiple_devices_migration_blocker);
@@ -410,7 +419,7 @@ int vfio_block_multiple_devices_migration(VFIODevice *vbasedev, Error **errp)
 void vfio_unblock_multiple_devices_migration(void)
 {
     if (!multiple_devices_migration_blocker ||
-        vfio_migratable_device_num() > 1) {
+        !vfio_should_block_non_p2p_migration()) {
         return;
     }
 
