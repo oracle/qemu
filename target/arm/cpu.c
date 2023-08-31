ec346181c1ff669a0aadf39e3207c9244dd0cce0
Richard Henderson <richard.henderson@linaro.org>
2023-08-31 09:45:15 +0100

    target/arm: When tag memory is not present, set MTE=1
    When the cpu support MTE, but the system does not, reduce cpu
    support to user instructions at EL0 instead of completely
    disabling MTE.  If we encounter a cpu implementation which does
    something else, we can revisit this setting.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-id: 20230811214031.171020-5-richard.henderson@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    (cherry picked from commit cd305b5f311d6ecea6cf487f3ec78b84bcd60d63)
    
    JIRA: https://jira.oci.oraclecorp.com/browse/LINUX-16020
    
    Conflicts:
    	target/arm/cpu.c
    
    Contextual - missing:
    7134cb07b749 ("target/arm: Support more GM blocksizes")
    
    Signed-off-by: Mark Kanda <mark.kanda@oracle.com>
    Reviewed-by: Karl Heubaum <karl.heubaum@oracle.com>

--- target/arm/cpu.c
+++ target/arm/cpu.c
@@ -1973,11 +1973,12 @@ static void arm_cpu_realizefn(DeviceState *dev, Error **errp)
 #ifndef CONFIG_USER_ONLY
     if (cpu->tag_memory == NULL && cpu_isar_feature(aa64_mte, cpu)) {
         /*
-         * Disable the MTE feature bits if we do not have tag-memory
-         * provided by the machine.
+         * If we do not have tag-memory provided by the machine,
+         * reduce MTE support to instructions enabled at EL0.
+         * This matches Cortex-A710 BROADCASTMTE input being LOW.
          */
         cpu->isar.id_aa64pfr1 =
-            FIELD_DP64(cpu->isar.id_aa64pfr1, ID_AA64PFR1, MTE, 0);
+                FIELD_DP64(cpu->isar.id_aa64pfr1, ID_AA64PFR1, MTE, 1);
     }
 #endif
 
