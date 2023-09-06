380c8116de4ddbe91a33359497da315a7febb000
Avihai Horon <avihaih@nvidia.com>
2023-09-06 18:08:50 +0300

    migration: Move more initializations to migrate_init()
    Initialization of mig_stats, compression_counters and VFIO bytes
    transferred is hard-coded in migration code path and snapshot code path.
    
    Make the code cleaner by initializing them in migrate_init().
    
    Suggested-by: Cédric Le Goater <clg@redhat.com>
    Signed-off-by: Avihai Horon <avihaih@nvidia.com>
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    Signed-off-by: Cédric Le Goater <clg@redhat.com>
    (cherry picked from commit f543aa222da183ac37424d1ea3a65e5fb6202732)
    Conflicts:
    	migration/migration.c
    	migration/savevm.c
    
    We store the stats in ram_counters rather than mig_stats.
    
    JIRA: https://jira.oci.oraclecorp.com/browse/LINUX-14257
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Reviewed-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>

--- migration/migration.c
+++ migration/migration.c
@@ -2193,6 +2193,13 @@ void migrate_init(MigrationState *s)
     s->iteration_initial_bytes = 0;
     s->threshold_size = 0;
     s->switchover_acked = false;
+    /*
+     * set ram_counters compression_counters memory to zero for a
+     * new migration
+     */
+    memset(&ram_counters, 0, sizeof(ram_counters));
+    memset(&compression_counters, 0, sizeof(compression_counters));
+    migration_reset_vfio_bytes_transferred();
 }
 
 int migrate_add_blocker_internal(Error *reason, Error **errp)
@@ -2404,13 +2411,6 @@ static bool migrate_prepare(MigrationState *s, bool blk, bool blk_inc,
     }
 
     migrate_init(s);
-    /*
-     * set ram_counters compression_counters memory to zero for a
-     * new migration
-     */
-    memset(&ram_counters, 0, sizeof(ram_counters));
-    memset(&compression_counters, 0, sizeof(compression_counters));
-    migration_reset_vfio_bytes_transferred();
 
     return true;
 }
