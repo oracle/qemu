a16e33929dc1083c0d84037bf8eea3059e457978
Leonardo Bras <leobras@redhat.com>
2023-10-12 11:16:26 -0700

    migration/multifd: Change multifd_load_cleanup() signature and usage
    Since it's introduction in commit f986c3d256 ("migration: Create multifd
    migration threads"), multifd_load_cleanup() never returned any value
    different than 0, neither set up any error on errp.
    
    Even though, on process_incoming_migration_bh() an if clause uses it's
    return value to decide on setting autostart = false, which will never
    happen.
    
    In order to simplify the codebase, change multifd_load_cleanup() signature
    to 'void multifd_load_cleanup(void)', and for every usage remove error
    handling or decision made based on return value != 0.
    
    Fixes: b5eea99ec2 ("migration: Add yank feature")
    Reported-by: Li Xiaohui <xiaohli@redhat.com>
    Signed-off-by: Leonardo Bras <leobras@redhat.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    (cherry picked from commit e5bac1f525472d6042a4cdba31dda5825cde0086)
    
     Conflicts:
    	migration/multifd.c
            We still check for migrate_multi_channels_is_allowed()
            in multifd_load_cleanup but it was removed by the
            commit d6f74fd12e464325f260d157c221e29480c62368
            "migration: Rework multi-channel checks on URI" which
            we do not have.
    
    Orabug: 35829153
    
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>

--- migration/multifd.c
+++ migration/multifd.c
@@ -1017,12 +1017,12 @@ static void multifd_recv_terminate_threads(Error *err)
     }
 }
 
-int multifd_load_cleanup(Error **errp)
+void multifd_load_cleanup(void)
 {
     int i;
 
     if (!migrate_use_multifd() || !migrate_multi_channels_is_allowed()) {
-        return 0;
+        return;
     }
     multifd_recv_terminate_threads(NULL);
     for (i = 0; i < migrate_multifd_channels(); i++) {
@@ -1062,8 +1062,6 @@ int multifd_load_cleanup(Error **errp)
     multifd_recv_state->params = NULL;
     g_free(multifd_recv_state);
     multifd_recv_state = NULL;
-
-    return 0;
 }
 
 void multifd_recv_sync_main(void)
