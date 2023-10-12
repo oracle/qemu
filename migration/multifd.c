c3ef2835bda927ec286dcbd2ba44584293431eeb
Leonardo Bras <leobras@redhat.com>
2023-10-12 11:16:29 -0700

    migration/multifd: Move load_cleanup inside incoming_state_destroy
    Currently running migration_incoming_state_destroy() without first running
    multifd_load_cleanup() will cause a yank error:
    
    qemu-system-x86_64: ../util/yank.c:107: yank_unregister_instance:
    Assertion `QLIST_EMPTY(&entry->yankfns)' failed.
    (core dumped)
    
    The above error happens in the target host, when multifd is being used
    for precopy, and then postcopy is triggered and the migration finishes.
    This will crash the VM in the target host.
    
    To avoid that, move multifd_load_cleanup() inside
    migration_incoming_state_destroy(), so that the load cleanup becomes part
    of the incoming state destroying process.
    
    Running multifd_load_cleanup() twice can become an issue, though, but the
    only scenario it could be ran twice is on process_incoming_migration_bh().
    So removing this extra call is necessary.
    
    On the other hand, this multifd_load_cleanup() call happens way before the
    migration_incoming_state_destroy() and having this happening before
    dirty_bitmap_mig_before_vm_start() and vm_start() may be a need.
    
    So introduce a new function multifd_load_shutdown() that will mainly stop
    all multifd threads and close their QIOChannels. Then use this function
    instead of multifd_load_cleanup() to make sure nothing else is received
    before dirty_bitmap_mig_before_vm_start().
    
    Fixes: b5eea99ec2 ("migration: Add yank feature")
    Reported-by: Li Xiaohui <xiaohli@redhat.com>
    Signed-off-by: Leonardo Bras <leobras@redhat.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    (cherry picked from commit cfc3bcf373218fb8757b0ff1ce2017b9b6ad4bff)
    
    Orabug: 35829153
    
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>

--- migration/multifd.c
+++ migration/multifd.c
@@ -1017,6 +1017,13 @@ static void multifd_recv_terminate_threads(Error *err)
     }
 }
 
+void multifd_load_shutdown(void)
+{
+    if (migrate_use_multifd()) {
+        multifd_recv_terminate_threads(NULL);
+    }
+}
+
 void multifd_load_cleanup(void)
 {
     int i;
