c1717af4c134d7138a8934b8479ba750e643bf9e
Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
2023-11-15 13:32:16 +0000

    hv-balloon: implement pre-Glib 2.68 compatibility
    Implement compatibility with Glib versions before 2.68 (lacking the
    required GTree API) by copying the missing functions and declarations
    directly to hv-balloon.
    
    JIRA: https://jira.oci.oraclecorp.com/browse/LINUX-14430
    
    Signed-off-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
    Reviewed-by: Karl Heubaum <karl.heubaum@oracle.com>

--- hw/hyperv/hv-balloon-internal.h
+++ hw/hyperv/hv-balloon-internal.h
@@ -30,4 +30,184 @@
         _result;                                \
     })
 
+#if !GLIB_CHECK_VERSION(2,68,0)
+
+#if !GLIB_CHECK_VERSION(2,12,0)
+#error Glib version far too old
+#endif
+
+typedef struct _GTreeNode GTreeNode;
+
+struct _GTreeNode
+{
+  gpointer   key;         /* key for this node */
+  gpointer   value;       /* value stored at this node */
+  GTreeNode *left;        /* left subtree */
+  GTreeNode *right;       /* right subtree */
+  gint8      balance;     /* height (right) - height (left) */
+  guint8     left_child;
+  guint8     right_child;
+};
+
+struct _GTree
+{
+  GTreeNode        *root;
+  GCompareDataFunc  key_compare;
+  GDestroyNotify    key_destroy_func;
+  GDestroyNotify    value_destroy_func;
+  gpointer          key_compare_data;
+  guint             nnodes;
+  gint              ref_count;
+};
+
+static inline gpointer
+g_tree_node_value (GTreeNode *node)
+{
+  g_return_val_if_fail (node != NULL, NULL);
+
+  return node->value;
+}
+
+static inline GTreeNode *
+g_tree_node_first (GTree *tree)
+{
+  GTreeNode *tmp;
+
+  g_return_val_if_fail (tree != NULL, NULL);
+
+  if (!tree->root)
+    return NULL;
+
+  tmp = tree->root;
+
+  while (tmp->left_child)
+    tmp = tmp->left;
+
+  return tmp;
+}
+
+static inline GTreeNode *
+g_tree_node_last (GTree *tree)
+{
+  GTreeNode *tmp;
+
+  g_return_val_if_fail (tree != NULL, NULL);
+
+  if (!tree->root)
+    return NULL;
+
+  tmp = tree->root;
+
+  while (tmp->right_child)
+    tmp = tmp->right;
+
+  return tmp;
+}
+
+static inline GTreeNode *
+g_tree_node_previous (GTreeNode *node)
+{
+  GTreeNode *tmp;
+
+  g_return_val_if_fail (node != NULL, NULL);
+
+  tmp = node->left;
+
+  if (node->left_child)
+    while (tmp->right_child)
+      tmp = tmp->right;
+
+  return tmp;
+}
+
+static inline GTreeNode *
+g_tree_node_next (GTreeNode *node)
+{
+  GTreeNode *tmp;
+
+  g_return_val_if_fail (node != NULL, NULL);
+
+  tmp = node->right;
+
+  if (node->right_child)
+    while (tmp->left_child)
+      tmp = tmp->left;
+
+  return tmp;
+}
+
+static inline GTreeNode *
+g_tree_upper_bound (GTree         *tree,
+                    gconstpointer  key)
+{
+  GTreeNode *node, *result;
+  gint cmp;
+
+  g_return_val_if_fail (tree != NULL, NULL);
+
+  node = tree->root;
+  if (!node)
+    return NULL;
+
+  result = NULL;
+  while (1)
+    {
+      cmp = tree->key_compare (key, node->key, tree->key_compare_data);
+      if (cmp < 0)
+        {
+          result = node;
+
+          if (!node->left_child)
+            return result;
+
+          node = node->left;
+        }
+      else
+        {
+          if (!node->right_child)
+            return result;
+
+          node = node->right;
+        }
+    }
+}
+
+/*
+ * Have to re-implement this as insert + lookup as there's no way to access
+ * the internal GTree insert + rebalance function.
+ */
+static inline GTreeNode *
+g_tree_insert_node (GTree    *tree,
+                    gpointer  key,
+                    gpointer  value)
+{
+  GTreeNode *node;
+  gint cmp;
+
+  g_tree_insert (tree, key, value);
+
+  node = tree->root;
+  assert (node);
+
+  while (1)
+    {
+      cmp = tree->key_compare (key, node->key, tree->key_compare_data);
+      if (cmp == 0)
+        return node;
+      else if (cmp < 0)
+        {
+          assert (node->left_child);
+          node = node->left;
+        }
+      else
+        {
+          assert (node->right_child);
+          node = node->right;
+        }
+    }
+
+  assert (false);
+}
+#endif
+
 #endif
