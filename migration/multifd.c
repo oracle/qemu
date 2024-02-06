b548329fe3ef699ba86866f2ad95a451a6a62f0e
Fabiano Rosas <farosas@suse.de>
2024-02-06 18:51:14 -0300

    migration/multifd: Remove p->running
    We currently only need p->running to avoid calling qemu_thread_join()
    on a non existent thread if the thread has never been created.
    
    However, there are at least two bugs in this logic:
    
    1) On the sending side, p->running is set too early and
    qemu_thread_create() can be skipped due to an error during TLS
    handshake, leaving the flag set and leading to a crash when
    multifd_send_cleanup() calls qemu_thread_join().
    
    2) During exit, the multifd thread clears the flag while holding the
    channel lock. The counterpart at multifd_send_cleanup() reads the flag
    outside of the lock and might free the mutex while the multifd thread
    still has it locked.
    
    Fix the first issue by setting the flag right before creating the
    thread. Rename it from p->running to p->thread_created to clarify its
    usage.
    
    Fix the second issue by not clearing the flag at the multifd thread
    exit. We don't have any use for that.
    
    Note that these bugs are straight-forward logic issues and not race
    conditions. There is still a gap for races to affect this code due to
    multifd_send_cleanup() being allowed to run concurrently with the
    thread creation loop. This issue is solved in the next patches.
    
    Cc: qemu-stable <qemu-stable@nongnu.org>
    Fixes: 29647140157a ("migration/tls: add support for multifd tls-handshake")
    Reported-by: Avihai Horon <avihaih@nvidia.com>
    Reported-by: chenyuhui5@huawei.com
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    Link: https://lore.kernel.org/r/20240206215118.6171-3-farosas@suse.de
    Signed-off-by: Peter Xu <peterx@redhat.com>
    (cherry picked from commit a2a63c4abd52f4e3ff4046dcb67fe44ebf0bb8de)
    
    Conflicts:
    	migration/multifd.c
            We dont have bca762c2b90979e8cf1f725cec3ddf888b7b898b
            "multifd: Remove some redundant code" so leave the code
            semantic as it is in multifd_new_send_channel_async() with
            removal of p->running and replacing with p->thread_created = true.
    
    Orabug: 34131170
    
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>

--- migration/multifd.c
+++ migration/multifd.c
@@ -634,7 +634,7 @@ static void multifd_send_terminate_threads(void)
             qemu_thread_join(&p->tls_thread);
         }
 
-        if (p->running) {
+        if (p->thread_created) {
             qemu_thread_join(&p->thread);
         }
     }
@@ -865,7 +865,6 @@ out:
         error_free(local_err);
     }
 
-    p->running = false;
     rcu_unregister_thread();
     trace_multifd_send_thread_end(p->id, p->packets_sent, p->total_normal_pages);
 
@@ -955,6 +954,7 @@ static bool multifd_channel_connect(MultiFDSendParams *p,
         migration_ioc_register_yank(ioc);
         p->registered_yank = true;
         p->c = ioc;
+        p->thread_created = true;
         qemu_thread_create(&p->thread, p->name, multifd_send_thread, p,
                            QEMU_THREAD_JOINABLE);
     }
@@ -972,7 +972,6 @@ static void multifd_new_send_channel_async(QIOTask *task, gpointer opaque)
         goto cleanup;
     } else {
         qio_channel_set_delay(sioc, false);
-        p->running = true;
         if (multifd_channel_connect(p, sioc, &local_err)) {
             return;
         }
@@ -1135,15 +1134,15 @@ void multifd_recv_cleanup(void)
     for (i = 0; i < migrate_multifd_channels(); i++) {
         MultiFDRecvParams *p = &multifd_recv_state->params[i];
 
-        if (p->running) {
-            /*
-             * multifd_recv_thread may hung at MULTIFD_FLAG_SYNC handle code,
-             * however try to wakeup it without harm in cleanup phase.
-             */
-            qemu_sem_post(&p->sem_sync);
-        }
+        /*
+         * multifd_recv_thread may hung at MULTIFD_FLAG_SYNC handle code,
+         * however try to wakeup it without harm in cleanup phase.
+         */
+        qemu_sem_post(&p->sem_sync);
 
-        qemu_thread_join(&p->thread);
+        if (p->thread_created) {
+            qemu_thread_join(&p->thread);
+        }
     }
     for (i = 0; i < migrate_multifd_channels(); i++) {
         multifd_recv_cleanup_channel(&multifd_recv_state->params[i]);
@@ -1232,9 +1231,6 @@ static void *multifd_recv_thread(void *opaque)
         multifd_recv_terminate_threads(local_err);
         error_free(local_err);
     }
-    qemu_mutex_lock(&p->mutex);
-    p->running = false;
-    qemu_mutex_unlock(&p->mutex);
 
     rcu_unregister_thread();
     trace_multifd_recv_thread_end(p->id, p->packets_recved, p->total_normal_pages);
@@ -1346,7 +1342,7 @@ void multifd_recv_new_channel(QIOChannel *ioc, Error **errp)
     p->c = ioc;
     object_ref(OBJECT(ioc));
 
-    p->running = true;
+    p->thread_created = true;
     qemu_thread_create(&p->thread, p->name, multifd_recv_thread, p,
                        QEMU_THREAD_JOINABLE);
     qatomic_inc(&multifd_recv_state->count);
