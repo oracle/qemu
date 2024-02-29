54bffd4c278a8f382900fd94100d7c0b7ba9f0c8
Fabiano Rosas <farosas@suse.de>
2024-02-29 12:30:06 -0300

    migration/multifd: Rename MultiFDSend|RecvParams::data to compress_data
    Use a more specific name for the compression data so we can use the
    generic for the multifd core code.
    
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    Link: https://lore.kernel.org/r/20240229153017.2221-13-farosas@suse.de
    Signed-off-by: Peter Xu <peterx@redhat.com>
    (cherry picked from commit 402dd7ac1c3be44f306c903cdfd2583ffec5e2fd)
    
    JIRA: https://jira.oci.oraclecorp.com/browse/LINUX-15752
    
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
    Signed-off-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>

--- migration/multifd.h
+++ migration/multifd.h
@@ -141,7 +141,7 @@ typedef struct {
     /* number of iovs used */
     uint32_t iovs_num;
     /* used for compression methods */
-    void *data;
+    void *compress_data;
 }  MultiFDSendParams;
 
 typedef struct {
@@ -203,7 +203,7 @@ typedef struct {
     /* num of zero pages */
     uint32_t zero_num;
     /* used for de-compression methods */
-    void *data;
+    void *compress_data;
 } MultiFDRecvParams;
 
 typedef struct {
