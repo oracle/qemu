6ed9ed28b490b68622ae507dd4f5bfc0f9fb5de5
Kevin Wolf <kwolf@redhat.com>
2024-09-03 11:25:55 -0400

    scsi-block: Don't skip callback for sgio error status/driver_status
    Instead of calling into scsi_handle_rw_error() directly from
    scsi_block_sgio_complete() and skipping the normal callback, go through
    the normal cleanup path by calling the callback with a positive error
    value.
    
    The important difference here is not only that the code path is cleaner,
    but that the callbacks set r->req.aiocb = NULL. If we skip setting this
    and the error action is BLOCK_ERROR_ACTION_STOP, resuming the VM runs
    into an assertion failure in scsi_read_data() or scsi_write_data()
    because the dangling aiocb pointer is unexpected.
    
    Fixes: a108557bbf ("scsi: inline sg_io_sense_from_errno() into the callers.")
    Buglink: https://issues.redhat.com/browse/RHEL-50000
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-ID: <20240731123207.27636-3-kwolf@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit 622a70161ac258e4a166a7dca4b5be267e0652d9)
    
    Conflicts:
              hw/scsi/scsi-disk.c
              - context as we do not have commit
                4f36b1384756914a6c4ffe04080a96da149c4c03
                "scsi: remove AioContext locking"
              - context as we do not have commit
                14042268045596a89f3fae3606389536d4784113
                "scsi: don't lock AioContext in I/O code path"
    
    JIRA: https://jira.oci.oraclecorp.com/browse/LINUX-15260
    
    Signed-off-by: Annie Li <annie.li@oracle.com>
    Reviewed-by: Mark Kanda <mark.kanda@oracle.com>

--- hw/scsi/scsi-disk.c
+++ hw/scsi/scsi-disk.c
@@ -2784,7 +2784,6 @@ static void scsi_block_sgio_complete(void *opaque, int ret)
 {
     SCSIBlockReq *req = (SCSIBlockReq *)opaque;
     SCSIDiskReq *r = &req->req;
-    SCSIDevice *s = r->req.dev;
     sg_io_hdr_t *io_hdr = &req->io_header;
 
     if (ret == 0) {
@@ -2798,19 +2797,6 @@ static void scsi_block_sgio_complete(void *opaque, int ret)
             ret = BUSY;
         } else {
             ret = io_hdr->status;
-        }
-
-        if (ret > 0) {
-            aio_context_acquire(blk_get_aio_context(s->conf.blk));
-            if (scsi_handle_rw_error(r, ret, true)) {
-                aio_context_release(blk_get_aio_context(s->conf.blk));
-                scsi_req_unref(&r->req);
-                return;
-            }
-            aio_context_release(blk_get_aio_context(s->conf.blk));
-
-            /* Ignore error.  */
-            ret = 0;
         }
     }
 
