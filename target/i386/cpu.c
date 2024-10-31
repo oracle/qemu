92c27d8d1907c3f36cb1f02d5d82f0c99ee0047d
Tao Su <tao1.su@linux.intel.com>
2024-10-31 16:52:31 +0800

    target/i386: Add feature dependencies for AVX10
    Since the highest supported vector length for a processor implies that
    all lesser vector lengths are also supported, add the dependencies of
    the supported vector lengths. If all vector lengths aren't supported,
    clear AVX10 enable bit as well.
    
    Note that the order of AVX10 related dependencies should be kept as:
            CPUID_24_0_EBX_AVX10_128     -> CPUID_24_0_EBX_AVX10_256,
            CPUID_24_0_EBX_AVX10_256     -> CPUID_24_0_EBX_AVX10_512,
            CPUID_24_0_EBX_AVX10_VL_MASK -> CPUID_7_1_EDX_AVX10,
            CPUID_7_1_EDX_AVX10          -> CPUID_24_0_EBX,
    so that prevent user from setting weird CPUID combinations, e.g. 256-bits
    and 512-bits are supported but 128-bits is not, no vector lengths are
    supported but AVX10 enable bit is still set.
    
    Since AVX10_128 will be reserved as 1, adding these dependencies has the
    bonus that when user sets -cpu host,-avx10-128, CPUID_7_1_EDX_AVX10 and
    CPUID_24_0_EBX will be disabled automatically.
    
    Tested-by: Xuelian Guo <xuelian.guo@intel.com>
    Signed-off-by: Tao Su <tao1.su@linux.intel.com>
    Link: https://lore.kernel.org/r/20241028024512.156724-5-tao1.su@linux.intel.com
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Link: https://lore.kernel.org/r/20241031085233.425388-7-tao1.su@linux.intel.com
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 150ab84b2d0083e6af344cca70290614d4fe568d)
    
    Orabug: 38330786
    
    Conflicts:
    	target/i386/cpu.c
    
    Contextual missing:
    c1acad9f72d1 ("target/i386: add support for FRED in CPUID enumeration")
    4912d6990b80 ("target/i386/cpu: Explicitly express SGX_LC and SGX feature words dependency")
    
    Signed-off-by: Mark Kanda <mark.kanda@oracle.com>
    Reviewed-by: Karl Heubaum <karl.heubaum@oracle.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -1492,6 +1492,22 @@ static FeatureDep feature_dependencies[] = {
         .from = { FEAT_7_0_ECX,             CPUID_7_0_ECX_WAITPKG },
         .to = { FEAT_VMX_SECONDARY_CTLS,    VMX_SECONDARY_EXEC_ENABLE_USER_WAIT_PAUSE },
     },
+    {
+        .from = { FEAT_24_0_EBX,            CPUID_24_0_EBX_AVX10_128 },
+        .to = { FEAT_24_0_EBX,              CPUID_24_0_EBX_AVX10_256 },
+    },
+    {
+        .from = { FEAT_24_0_EBX,            CPUID_24_0_EBX_AVX10_256 },
+        .to = { FEAT_24_0_EBX,              CPUID_24_0_EBX_AVX10_512 },
+    },
+    {
+        .from = { FEAT_24_0_EBX,            CPUID_24_0_EBX_AVX10_VL_MASK },
+        .to = { FEAT_7_1_EDX,               CPUID_7_1_EDX_AVX10 },
+    },
+    {
+        .from = { FEAT_7_1_EDX,             CPUID_7_1_EDX_AVX10 },
+        .to = { FEAT_24_0_EBX,              ~0ull },
+    },
 };
 
 typedef struct X86RegisterInfo32 {
