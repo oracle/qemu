0df45e38cb59ff2b7e5ab7578446a584a6019cf6
Tao Su <tao1.su@linux.intel.com>
2024-10-31 16:52:30 +0800

    target/i386: add CPUID.24 features for AVX10
    Introduce features for the supported vector bit lengths.
    
    Signed-off-by: Tao Su <tao1.su@linux.intel.com>
    Link: https://lore.kernel.org/r/20241028024512.156724-3-tao1.su@linux.intel.com
    Link: https://lore.kernel.org/r/20241028024512.156724-4-tao1.su@linux.intel.com
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Tested-by: Xuelian Guo <xuelian.guo@intel.com>
    Link: https://lore.kernel.org/r/20241031085233.425388-6-tao1.su@linux.intel.com
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 2d055b8fe11ee567c2ae8047311fd83697e494b6)
    
    Orabug: 38330786
    
    Signed-off-by: Mark Kanda <mark.kanda@oracle.com>
    Reviewed-by: Steve Sistare <steven.sistare@oracle.com>
    Reviewed-by: Karl Heubaum <karl.heubaum@oracle.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -680,6 +680,7 @@ void x86_cpu_vendor_words2str(char *dst, uint32_t vendor1,
 #define TCG_SGX_12_0_EAX_FEATURES 0
 #define TCG_SGX_12_0_EBX_FEATURES 0
 #define TCG_SGX_12_1_EAX_FEATURES 0
+#define TCG_24_0_EBX_FEATURES 0
 
 FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
     [FEAT_1_EDX] = {
@@ -931,6 +932,20 @@ FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
             .reg = R_EDX,
         },
         .tcg_features = TCG_7_2_EDX_FEATURES,
+    },
+    [FEAT_24_0_EBX] = {
+        .type = CPUID_FEATURE_WORD,
+        .feat_names = {
+            [16] = "avx10-128",
+            [17] = "avx10-256",
+            [18] = "avx10-512",
+        },
+        .cpuid = {
+            .eax = 0x24,
+            .needs_ecx = true, .ecx = 0,
+            .reg = R_EBX,
+        },
+        .tcg_features = TCG_24_0_EBX_FEATURES,
     },
     [FEAT_8000_0007_EDX] = {
         .type = CPUID_FEATURE_WORD,
