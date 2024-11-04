9aede080956f6798fc78e3ad4f838095c4987203
Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
2024-11-04 22:29:07 +0100

    vfio/migration: Add vfio_save_block_precopy_empty_hit trace event
    This way it is clearly known when there's no more data to send for that
    device.
    
    (cherry picked from commit 89b516152777a8b54b117d90690ed9be62ba1177)
    
    JIRA: https://jira.oci.oraclecorp.com/browse/LINUX-15752
    
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
    Signed-off-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -72,6 +72,7 @@ typedef struct VFIOMigration {
     bool initial_data_sent;
 
     bool event_save_iterate_started;
+    bool event_precopy_empty_hit;
 } VFIOMigration;
 
 typedef struct VFIOAddressSpace {
