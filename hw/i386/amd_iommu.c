bb641734def51022943afe6f2d08a569aee0bc9b
Sairaj Kodilkar <sarunkod@amd.com>
2024-11-29 22:59:05 +0000

    amd_iommu: Fix kvm_enable_x2apic link error with clang in non-KVM builds
    Commit b12cb3819 (amd_iommu: Check APIC ID > 255 for XTSup) throws
    linking error for the `kvm_enable_x2apic` when kvm is disabled
    and Clang is used for compilation.
    
    This issue comes up because Clang does not remove the function callsite
    (kvm_enable_x2apic in this case) during optimization when if condition
    have variable. Intel IOMMU driver solves this issue by creating separate
    if condition for checking variables, which causes call site being
    optimized away by virtue of `kvm_irqchip_is_split()` being defined as 0.
    Implement same solution for the AMD driver.
    
    Fixes: b12cb3819baf (amd_iommu: Check APIC ID > 255 for XTSup)
    Signed-off-by: Sairaj Kodilkar <sarunkod@amd.com>
    Signed-off-by: Santosh Shukla <santosh.shukla@amd.com>
    Tested-by: Phil Dennis-Jordan <phil@philjordan.eu>
    Link: https://lore.kernel.org/r/20241114114509.15350-1-sarunkod@amd.com
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 0266aef8cd6511e5de0c0cb818d02ae7814d4940)
    Orabug: 35710551
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>

--- hw/i386/amd_iommu.c
+++ hw/i386/amd_iommu.c
@@ -1653,9 +1653,11 @@ static void amdvi_sysbus_realize(DeviceState *dev, Error **errp)
         error_report("AMD IOMMU with x2APIC confguration requires xtsup=on");
         exit(EXIT_FAILURE);
     }
-    if (s->xtsup && kvm_irqchip_is_split() && !kvm_enable_x2apic()) {
-        error_report("AMD IOMMU xtsup=on requires support on the KVM side");
-        exit(EXIT_FAILURE);
+    if (s->xtsup) {
+        if (kvm_irqchip_is_split() && !kvm_enable_x2apic()) {
+            error_report("AMD IOMMU xtsup=on requires support on the KVM side");
+            exit(EXIT_FAILURE);
+        }
     }
 
     pci_setup_iommu(bus, amdvi_host_dma_iommu, s);
