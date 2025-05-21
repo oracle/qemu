oracle/qemu

commit bacd9942051de1d85ab134908bf664f4267e4f56

migration/dirtyrate: Add new mode to dirty track non KVM device separately
Expand dirtyrate measurer that is accessible via HMP calc_dirty_rate or
QMP 'calc-dirty-rate' to receive a new @mode argument. The new
x-orcl-dirty-bitmap-devices mode restricts the dirty tracking to be done
at non-KVM devices only, amd hence without using the KVM (CPU) dirty
tracker. The default stays as is i.e. dirty-ring / dirty-bitmap from KVM.

This is useful to test, exercise the IOMMU dirty tracker and observe
how much a given VF/vhost device is dirtying memory.

Orabug: 37097510

Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
Tested-by: Partha Tirumalai <partha.tirumalai@oracle.com>
Reviewed-by: Karl Heubaum <karl.heubaum@oracle.com>

accel/kvm/kvm-all.c
@@ -1566,6 +1566,10 @@ static void kvm_log_sync(MemoryListener *listener,
 {
     KVMMemoryListener *kml = container_of(listener, KVMMemoryListener, listener);
 
+    if (memory_global_dirty_devices()) {
+        return;
+    }
+
     kvm_slots_lock();
     kvm_physical_sync_dirty_bitmap(kml, section);
     kvm_slots_unlock();
@@ -1577,6 +1581,10 @@ static void kvm_log_sync_global(MemoryListener *l)
     KVMState *s = kvm_state;
     KVMSlot *mem;
     int i;
+
+    if (memory_global_dirty_devices()) {
+        return;
+    }
 
     /* Flush all kernel dirty addresses into KVMSlot dirty bitmap */
     kvm_dirty_ring_flush();
@@ -1606,6 +1614,10 @@ static void kvm_log_clear(MemoryListener *listener,
 {
     KVMMemoryListener *kml = container_of(listener, KVMMemoryListener, listener);
     int r;
+
+    if (memory_global_dirty_devices()) {
+        return;
+    }
 
     r = kvm_physical_log_clear(kml, section);
     if (r < 0) {

hmp-commands.hx
@@ -1744,11 +1744,12 @@ ERST
 
     {
         .name       = "calc_dirty_rate",
-        .args_type  = "dirty_ring:-r,dirty_bitmap:-b,second:l,sample_pages_per_GB:l?",
-        .params     = "[-r] [-b] second [sample_pages_per_GB]",
+        .args_type  = "dirty_devices:-d,dirty_ring:-r,dirty_bitmap:-b,second:l,sample_pages_per_GB:l?",
+        .params     = "[-d] [-r] [-b] second [sample_pages_per_GB]",
         .help       = "start a round of guest dirty rate measurement (using -r to"
                       "\n\t\t\t specify dirty ring as the method of calculation and"
-                      "\n\t\t\t -b to specify dirty bitmap as method of calculation)",
+                      "\n\t\t\t -b to specify dirty bitmap as method of calculation and"
+                      "\n\t\t\t -d to specify dirty bitmap but restricted to non-CPU devices)",
         .cmd        = hmp_calc_dirty_rate,
     },
 

include/exec/memory.h
@@ -72,7 +72,10 @@ static inline void fuzz_dma_read_cb(size_t addr,
 /* Dirty tracking enabled because dirty limit */
 #define GLOBAL_DIRTY_LIMIT      (1U << 2)
 
-#define GLOBAL_DIRTY_MASK  (0x7)
+/* Dirty tracking enabled because measuring devices dirty rate */
+#define GLOBAL_DIRTY_DIRTY_RATE_DEVICES (1U << 3)
+
+#define GLOBAL_DIRTY_MASK  (0xF)
 
 extern unsigned int global_dirty_tracking;
 
@@ -2467,6 +2470,11 @@ void memory_global_dirty_log_start(unsigned int flags);
  * @flags: purpose of stopping dirty log, migration or dirty rate
  */
 void memory_global_dirty_log_stop(unsigned int flags);
+
+/**
+ * memory_global_dirty_devices: check if the scope is just devices
+ */
+bool memory_global_dirty_devices(void);
 
 void mtree_info(bool flatview, bool dispatch_tree, bool owner, bool disabled);
 

migration/dirtyrate.c
@@ -252,7 +252,8 @@ static struct DirtyRateInfo *query_dirty_rate_info(void)
             info->vcpu_dirty_rate = head;
         }
 
-        if (dirtyrate_mode == DIRTY_RATE_MEASURE_MODE_DIRTY_BITMAP) {
+        if (dirtyrate_mode == DIRTY_RATE_MEASURE_MODE_DIRTY_BITMAP ||
+            dirtyrate_mode == DIRTY_RATE_MEASURE_MODE_X_ORCL_DIRTY_BITMAP_DEVICES) {
             info->sample_pages = 0;
         }
     }
@@ -581,9 +582,14 @@ static void calculate_dirtyrate_dirty_bitmap(struct DirtyRateConfig config)
     int64_t msec = 0;
     int64_t start_time;
     DirtyPageRecord dirty_pages;
+    unsigned int flags = GLOBAL_DIRTY_DIRTY_RATE;
+
+    if (config.mode == DIRTY_RATE_MEASURE_MODE_X_ORCL_DIRTY_BITMAP_DEVICES) {
+        flags |= GLOBAL_DIRTY_DIRTY_RATE_DEVICES;
+    }
 
     qemu_mutex_lock_iothread();
-    memory_global_dirty_log_start(GLOBAL_DIRTY_DIRTY_RATE);
+    memory_global_dirty_log_start(flags);
 
     /*
      * 1'round of log sync may return all 1 bits with
@@ -615,7 +621,7 @@ static void calculate_dirtyrate_dirty_bitmap(struct DirtyRateConfig config)
      * 1. fetch dirty bitmap from kvm
      * 2. stop dirty tracking
      */
-    global_dirty_log_sync(GLOBAL_DIRTY_DIRTY_RATE, true);
+    global_dirty_log_sync(flags, true);
 
     record_dirtypages_bitmap(&dirty_pages, false);
 
@@ -685,7 +691,8 @@ out:
 
 static void calculate_dirtyrate(struct DirtyRateConfig config)
 {
-    if (config.mode == DIRTY_RATE_MEASURE_MODE_DIRTY_BITMAP) {
+    if (config.mode == DIRTY_RATE_MEASURE_MODE_DIRTY_BITMAP ||
+        config.mode == DIRTY_RATE_MEASURE_MODE_X_ORCL_DIRTY_BITMAP_DEVICES) {
         calculate_dirtyrate_dirty_bitmap(config);
     } else if (config.mode == DIRTY_RATE_MEASURE_MODE_DIRTY_RING) {
         calculate_dirtyrate_dirty_ring(config);
@@ -855,6 +862,7 @@ void hmp_calc_dirty_rate(Monitor *mon, const QDict *qdict)
     bool has_sample_pages = (sample_pages != -1);
     bool dirty_ring = qdict_get_try_bool(qdict, "dirty_ring", false);
     bool dirty_bitmap = qdict_get_try_bool(qdict, "dirty_bitmap", false);
+    bool dirty_devices = qdict_get_try_bool(qdict, "dirty_devices", false);
     DirtyRateMeasureMode mode = DIRTY_RATE_MEASURE_MODE_PAGE_SAMPLING;
     Error *err = NULL;
 
@@ -863,8 +871,9 @@ void hmp_calc_dirty_rate(Monitor *mon, const QDict *qdict)
         return;
     }
 
-    if (dirty_ring && dirty_bitmap) {
-        monitor_printf(mon, "Either dirty ring or dirty bitmap "
+    if ((dirty_ring && (dirty_bitmap ^ dirty_devices)) ||
+        (dirty_bitmap && dirty_devices)) {
+        monitor_printf(mon, "Either dirty ring or dirty bitmap or dirty devices "
                        "can be specified!\n");
         return;
     }
@@ -873,6 +882,8 @@ void hmp_calc_dirty_rate(Monitor *mon, const QDict *qdict)
         mode = DIRTY_RATE_MEASURE_MODE_DIRTY_BITMAP;
     } else if (dirty_ring) {
         mode = DIRTY_RATE_MEASURE_MODE_DIRTY_RING;
+    } else if (dirty_devices) {
+        mode = DIRTY_RATE_MEASURE_MODE_X_ORCL_DIRTY_BITMAP_DEVICES;
     }
 
     qmp_calc_dirty_rate(sec, has_sample_pages, sample_pages, true,

softmmu/memory.c
@@ -2935,6 +2935,11 @@ void memory_global_dirty_log_start(unsigned int flags)
     }
 }
 
+bool memory_global_dirty_devices(void)
+{
+    return !!(global_dirty_tracking & GLOBAL_DIRTY_DIRTY_RATE_DEVICES);
+}
+
 static void memory_global_dirty_log_do_stop(unsigned int flags)
 {
     assert(flags && !(flags & (~GLOBAL_DIRTY_MASK)));

commit ba8b783768af475bd3723aff90b85034bbe27e07

migration/dirtyrate: skip kvm_log_start/kvm_log_stop for non-KVM
The commit bacd9942051d ("migration/dirtyrate: Add new mode to dirty track
non KVM device separately") has already skipped kvm_log_sync(),
kvm_log_sync_global and kvm_log_clear(), for 'x-orcl-dirty-bitmap-devices'
mode.

However, kvm_log_start() and kvm_log_log() are missed, although they are
unnecessary in 'x-orcl-dirty-bitmap-devices' mode.

As a result, when the VM memory size is 640GB, "calc_dirty_rate -d 1" can
take up to 2.8 seconds between line 109 and line 114.

107 static void global_dirty_log_sync(unsigned int flag, bool one_shot)
108 {
109     qemu_mutex_lock_iothread();
110     memory_global_dirty_log_sync();
111     if (one_shot) {
112         memory_global_dirty_log_stop(flag);
113     }
114     qemu_mutex_unlock_iothread();
115 }

In the worst case, any of mainloop/RCU/vCPU threads can be stuck for 2.8
seconds, waiting for the unlock of QEMU global mutex by line 114.

Orabug: 37939813
Fixes: bacd9942051d ("migration/dirtyrate: Add new mode to dirty track non KVM device separately")
Suggested-by: Joao Martins <joao.m.martins@oracle.com>
Signed-off-by: Dongli Zhang <dongli.zhang@oracle.com>
Reviewed-by: Joao Martins <joao.m.martins@oracle.com>

accel/kvm/kvm-all.c
@@ -540,6 +540,10 @@ static void kvm_log_start(MemoryListener *listener,
     KVMMemoryListener *kml = container_of(listener, KVMMemoryListener, listener);
     int r;
 
+    if (memory_global_dirty_devices()) {
+        return;
+    }
+
     if (old != 0) {
         return;
     }
@@ -556,6 +560,10 @@ static void kvm_log_stop(MemoryListener *listener,
 {
     KVMMemoryListener *kml = container_of(listener, KVMMemoryListener, listener);
     int r;
+
+    if (memory_global_dirty_devices()) {
+        return;
+    }
 
     if (new != 0) {
         return;