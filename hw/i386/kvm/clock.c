oracle/qemu

commit 9dbc47c1fcec3bc013bed2d1f48a1ce7f8777787

target/i386/kvm: account blackout downtime for kvm-clock and guest TSC
So far, QEMU/KVM live migration does not account all elapsed blackout
downtimes. For example, if a guest is live-migrated to a file, left idle
for one hour, and then restored from that file to the target host, the
one-hour blackout period will not be reflected in the kvm-clock or guest
TSC.

Typically, the elapsed time between KVM_GET_CLOCK (on the source QEMU) and
KVM_SET_CLOCK (on the target QEMU) is not accounted in the kvm-clock.
Similarly, the elapsed time between reading MSR_IA32_TSC on the source QEMU
and writing it on the target QEMU is not reflected in the guest TSC.

The KVM patchset [1] introduced KVM_VCPU_TSC_CTRL, KVM_CLOCK_REALTIME, and
KVM_CLOCK_HOST_TSC to account the elapsed time during live migration
blackouts in the guest's system counter view.

The core idea is to use the realtime clock (KVM_CLOCK_REALTIME) from both
the source and target hosts as a reference to calculate the elapsed
downtime in nanoseconds and adjust kvm-clock. In addition, these
nanoseconds are converted into TSC cycles using the vCPU's virtual TSC
frequency, and the corresponding elapsed cycles are compensated in the
guest TSC as well (KVM_CLOCK_HOST_TSC and KVM_VCPU_TSC_CTRL).

The following steps are copied from the Linux kernel documentation [2].

From the source VMM process:

1. Invoke the KVM_GET_CLOCK ioctl to record the host TSC (tsc_src),
kvmclock nanoseconds (guest_src), and host CLOCK_REALTIME nanoseconds
(host_src).

2. Read the KVM_VCPU_TSC_OFFSET attribute for every vCPU to record the
guest TSC offset (ofs_src[i]).

3. Invoke the KVM_GET_TSC_KHZ ioctl to record the frequency of the guest's
TSC (freq).

From the destination VMM process:

4. Invoke the KVM_SET_CLOCK ioctl, providing the source nanoseconds from
kvmclock (guest_src) and CLOCK_REALTIME (host_src) in their respective
fields. Ensure that the KVM_CLOCK_REALTIME flag is set in the provided
structure.

hw/i386/kvm/clock.c
@@ -39,6 +39,9 @@ struct KVMClockState {
     /*< public >*/
 
     uint64_t clock;
+    uint64_t realtime;
+    uint64_t host_tsc;
+    uint32_t flags;
     bool clock_valid;
 
     /* whether the 'clock' value was obtained in the 'paused' state */
@@ -50,6 +53,12 @@ struct KVMClockState {
     /* whether the 'clock' value was obtained in a host with
      * reliable KVM_GET_CLOCK */
     bool clock_is_reliable;
+
+    /*
+     * whether to account downtimes by taking advantage of
+     * KVM_CLOCK_REALTIME, KVM_CLOCK_HOST_TSC and KVM_VCPU_TSC_CTRL.
+     */
+    bool account_downtime;
 };
 
 struct pvclock_vcpu_time_info {
@@ -101,6 +110,7 @@ static uint64_t kvmclock_current_nsec(KVMClockState *s)
 static void kvm_update_clock(KVMClockState *s)
 {
     struct kvm_clock_data data;
+    bool has_aux;
     int ret;
 
     ret = kvm_vm_ioctl(kvm_state, KVM_GET_CLOCK, &data);
@@ -109,6 +119,18 @@ static void kvm_update_clock(KVMClockState *s)
                 abort();
     }
     s->clock = data.clock;
+
+    s->realtime = 0;
+    s->host_tsc = 0;
+    s->flags = 0;
+
+    has_aux = with_kvmclock_aux_flags(data.flags);
+
+    if (s->account_downtime && kvm_support_clock_downtime() && has_aux) {
+        s->realtime = data.realtime;
+        s->host_tsc = data.host_tsc;
+        s->flags = data.flags & KVM_CLOCK_AUX_FLAGS;
+    }
 
     /* If kvm_has_adjust_clock_stable() is false, KVM_GET_CLOCK returns
      * essentially CLOCK_MONOTONIC plus a guest-specific adjustment.  This
@@ -161,6 +183,36 @@ static void do_kvmclock_ctrl(CPUState *cpu, run_on_cpu_data data)
     }
 }
 
+static void kvmclock_adjust_tsc_offset(KVMClockState *s)
+{
+    CPUX86State *env = first_cpu->env_ptr;
+    struct kvm_clock_data data;
+    uint64_t delta;
+    int ret;
+
+    ret = kvm_vm_ioctl(kvm_state, KVM_GET_CLOCK, &data);
+    if (ret < 0) {
+        fprintf(stderr, "KVM_GET_CLOCK failed: %s\n", strerror(-ret));
+        abort();
+    }
+
+    if (!with_kvmclock_aux_flags(data.flags)) {
+        fprintf(stderr, "warning: cannot adjust tsc offset\n");
+        return;
+    }
+
+    if ((int64_t)(data.clock - s->clock) < 0) {
+        fprintf(stderr, "warning: clock backward detected (%lu, %llu)\n",
+                s->clock, data.clock);
+        return;
+    }
+
+    delta = ((data.clock - s->clock) * env->tsc_khz) / 1000000 +
+            (s->host_tsc - data.host_tsc);
+
+    kvm_write_all_tsc_offset(delta);
+}
+
 static void kvmclock_vm_state_change(void *opaque, bool running,
                                      RunState state)
 {
@@ -171,6 +223,7 @@ static void kvmclock_vm_state_change(void *opaque, bool running,
 
     if (running) {
         struct kvm_clock_data data = {};
+        bool account_downtime;
 
         /*
          * If the host where s->clock was read did not support reliable
@@ -185,12 +238,25 @@ static void kvmclock_vm_state_change(void *opaque, bool running,
         }
 
         s->clock_valid = false;
-
         data.clock = s->clock;
+
+        account_downtime = s->account_downtime &&
+            kvm_support_clock_downtime() &&
+            with_kvmclock_aux_flags(s->flags);
+
+        if (account_downtime) {
+            data.realtime = s->realtime;
+            data.flags = s->flags & KVM_CLOCK_REALTIME;
+        }
+
         ret = kvm_vm_ioctl(kvm_state, KVM_SET_CLOCK, &data);
         if (ret < 0) {
             fprintf(stderr, "KVM_SET_CLOCK failed: %s\n", strerror(-ret));
             abort();
+        }
+
+        if (account_downtime) {
+            kvmclock_adjust_tsc_offset(s);
         }
 
         if (!cap_clock_ctrl) {
@@ -251,6 +317,26 @@ static const VMStateDescription kvmclock_reliable_get_clock = {
     }
 };
 
+static bool kvmclock_account_downtime(void *opaque)
+{
+    KVMClockState *s = opaque;
+
+    return s->account_downtime;
+}
+
+static const VMStateDescription kvmclock_auxiliary = {
+    .name = "kvmclock/auxiliary",
+    .version_id = 1,
+    .minimum_version_id = 1,
+    .needed = kvmclock_account_downtime,
+    .fields = (VMStateField[]) {
+        VMSTATE_UINT32(flags, KVMClockState),
+        VMSTATE_UINT64(realtime, KVMClockState),
+        VMSTATE_UINT64(host_tsc, KVMClockState),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
 /*
  * When migrating, assume the source has an unreliable
  * KVM_GET_CLOCK unless told otherwise.
@@ -260,6 +346,9 @@ static int kvmclock_pre_load(void *opaque)
     KVMClockState *s = opaque;
 
     s->clock_is_reliable = false;
+    s->realtime = 0;
+    s->host_tsc = 0;
+    s->flags = 0;
 
     return 0;
 }
@@ -301,6 +390,7 @@ static const VMStateDescription kvmclock_vmsd = {
     },
     .subsections = (const VMStateDescription * []) {
         &kvmclock_reliable_get_clock,
+        &kvmclock_auxiliary,
         NULL
     }
 };
@@ -308,6 +398,8 @@ static const VMStateDescription kvmclock_vmsd = {
 static Property kvmclock_properties[] = {
     DEFINE_PROP_BOOL("x-mach-use-reliable-get-clock", KVMClockState,
                       mach_use_reliable_get_clock, true),
+    DEFINE_PROP_BOOL("x-orcl-account-downtime", KVMClockState,
+                      account_downtime, false),
     DEFINE_PROP_END_OF_LIST(),
 };
 

hw/core/machine.c
@@ -50,6 +50,7 @@ GlobalProperty hw_compat_7_2_exadata[] = {
     { "vfio-pci", "migration-events", "true" },
     { "migration", "x-orcl-switchover-event", "on" },
     { "migration", "x-orcl-migrate-use-hash", "true" },
+    { "kvmclock", "x-orcl-account-downtime", "true" },
 };
 const size_t hw_compat_7_2_exadata_len = G_N_ELEMENTS(hw_compat_7_2_exadata);
 

target/i386/kvm/kvm.c
@@ -138,6 +138,9 @@ static int has_triple_fault_event;
 
 static bool has_msr_mcg_ext_ctl;
 
+static bool has_kvmclock_aux;
+static bool has_tsc_offset;
+
 static struct kvm_cpuid2 *cpuid_cache;
 static struct kvm_cpuid2 *hv_cpuid_cache;
 static struct kvm_msr_list *kvm_feature_msrs;
@@ -173,6 +176,11 @@ bool kvm_has_adjust_clock(void)
 bool kvm_has_exception_payload(void)
 {
     return has_exception_payload;
+}
+
+bool kvm_support_clock_downtime(void)
+{
+    return has_kvmclock_aux && has_tsc_offset;
 }
 
 static bool kvm_x2apic_api_set_flags(uint64_t flags)
@@ -220,6 +228,20 @@ static int kvm_get_tsc(CPUState *cs)
     uint64_t value;
     int ret;
 
+    if (kvm_support_clock_downtime()) {
+        struct kvm_device_attr attr;
+
+        attr.group = KVM_VCPU_TSC_CTRL;
+        attr.attr = KVM_VCPU_TSC_OFFSET;
+        attr.flags = 0;
+        attr.addr = (uint64_t)&env->tsc_offset;
+
+        ret = kvm_vcpu_ioctl(cs, KVM_GET_DEVICE_ATTR, &attr);
+        if (ret) {
+            return ret;
+        }
+    }
+
     if (env->tsc_valid) {
         return 0;
     }
@@ -247,6 +269,40 @@ void kvm_synchronize_all_tsc(void)
     if (kvm_enabled()) {
         CPU_FOREACH(cpu) {
             run_on_cpu(cpu, do_kvm_synchronize_tsc, RUN_ON_CPU_NULL);
+        }
+    }
+}
+
+static inline void do_kvm_write_tsc_offset(CPUState *cs, run_on_cpu_data arg)
+{
+    uint64_t delta = arg.host_ulong;
+    X86CPU *cpu = X86_CPU(cs);
+    CPUX86State *env = &cpu->env;
+    struct kvm_device_attr attr;
+    int ret;
+
+    env->tsc_offset += delta;
+
+    attr.group = KVM_VCPU_TSC_CTRL;
+    attr.attr = KVM_VCPU_TSC_OFFSET;
+    attr.flags = 0;
+    attr.addr = (uint64_t)&env->tsc_offset;
+
+    ret = kvm_vcpu_ioctl(cs, KVM_SET_DEVICE_ATTR, &attr);
+    if (ret) {
+        error_report("KVM_SET_DEVICE_ATTR: %s", strerror(-ret));
+        exit(1);
+    }
+}
+
+void kvm_write_all_tsc_offset(uint64_t delta)
+{
+    CPUState *cpu;
+
+    if (kvm_enabled()) {
+        CPU_FOREACH(cpu) {
+            run_on_cpu(cpu, do_kvm_write_tsc_offset,
+                       RUN_ON_CPU_HOST_ULONG(delta));
         }
     }
 }
@@ -2242,6 +2298,18 @@ int kvm_arch_init_vcpu(CPUState *cs)
 
     kvm_init_msrs(cpu);
 
+    if (cs == first_cpu) {
+        struct kvm_device_attr attr = {
+            .group = KVM_VCPU_TSC_CTRL,
+            .attr = KVM_VCPU_TSC_OFFSET,
+            .flags = 0,
+        };
+
+        if (!kvm_vcpu_ioctl(cs, KVM_HAS_DEVICE_ATTR, &attr)) {
+            has_tsc_offset = true;
+        }
+    }
+
     return 0;
 
  fail:
@@ -2742,6 +2810,11 @@ int kvm_arch_init(MachineState *ms, KVMState *s)
                          strerror(-ret));
             exit(1);
         }
+    }
+
+    ret = kvm_check_extension(s, KVM_CAP_ADJUST_CLOCK);
+    if (ret) {
+        has_kvmclock_aux = with_kvmclock_aux_flags(ret);
     }
 
     return 0;

target/i386/kvm/kvm_i386.h
@@ -32,11 +32,17 @@
 
 #endif  /* CONFIG_KVM */
 
+#define KVM_CLOCK_AUX_FLAGS (KVM_CLOCK_REALTIME | KVM_CLOCK_HOST_TSC)
+#define with_kvmclock_aux_flags(flags) \
+    ((flags & KVM_CLOCK_AUX_FLAGS) == KVM_CLOCK_AUX_FLAGS)
+
 bool kvm_has_smm(void);
 bool kvm_has_adjust_clock(void);
 bool kvm_has_adjust_clock_stable(void);
 bool kvm_has_exception_payload(void);
+bool kvm_support_clock_downtime(void);
 void kvm_synchronize_all_tsc(void);
+void kvm_write_all_tsc_offset(uint64_t delta);
 void kvm_arch_reset_vcpu(X86CPU *cs);
 void kvm_arch_after_reset_vcpu(X86CPU *cpu);
 void kvm_arch_do_init_vcpu(X86CPU *cs);

commit fcdd13df341f9b64e4eb7b00ef53d202231671d8

target/i386/kvm: implement reset method for kvmclock
If kvmclock data relevant to "x-orcl-account-downtime" is not cleared
during a system reset, performing a "virsh reboot" on a UEFI-based guest VM
that has previously experienced vCPU addition and then removal may cause
the guest TSC to become unsynchronized. As a result, the guest VM may
revert to using the global clock, which can significantly degrade performance.

1. Create and boot a SecureBoot-enabled guest VM with the parameters
"-smp 2,maxcpus=8", using UEFI/EDK2 firmware. Use the feature
"-global kvmclock.x-orcl-account-downtime=true".

2. Add two vCPUs to the VM, and then remove them.

(qemu) device_add host-x86_64-cpu,id=core2,socket-id=0,core-id=2,thread-id=0
(qemu) device_add host-x86_64-cpu,id=core3,socket-id=0,core-id=3,thread-id=0
(qemu) device_del core2
(qemu) device_del core3

3. Reboot the guest VM from QEMU, simulating a "virsh reboot".

(qemu) system_powerdown
(qemu) system_reset
(qemu) cont

4. The guest TSC is no longer synchronized.

hv# cat /sys/kernel/debug/kvm/<QEMU-PID>/vcpu*/tsc-offset
2422532
2422532
-486120163177647
-486120163177647

This issue can be reproduced with UEFI/EDK2-based guest VMs, but not with
BIOS-based guest VMs. According to observations from the guest OS, a
BIOS-based VM issues a reset during bootup after "cont".

Therefore, a reset method should be added for kvmclock to clear data
relevant to "x-orcl-account-downtime". This data will then become inactive
in kvmclock_vm_state_change() after a system reset.

Although the reset method is deprecated and has been replaced by methods
in the Resettable class interface for implementing multi-phase resets, it
is still commonly used by many devices in QEMU-7.2, such as virtio-pci
(e.g., virtio_pci_bus_reset()). Therefore, we continue to use the reset
method for kvmclock.

Orabug: 38853905
Fixes: 9dbc47c1fcec ("target/i386/kvm: account blackout downtime for kvm-clock and guest TSC")
Reported-by: Rose Wang <rose.wang@oracle.com>

hw/i386/kvm/clock.c
@@ -285,6 +285,19 @@ static void kvmclock_vm_state_change(void *opaque, bool running,
     }
 }
 
+static void kvmclock_reset(DeviceState *dev)
+{
+    KVMClockState *s = KVM_CLOCK(dev);
+
+    if (!kvm_enabled()) {
+        return;
+    }
+
+    s->flags = 0;
+    s->realtime = 0;
+    s->host_tsc = 0;
+}
+
 static void kvmclock_realize(DeviceState *dev, Error **errp)
 {
     KVMClockState *s = KVM_CLOCK(dev);
@@ -408,6 +421,7 @@ static void kvmclock_class_init(ObjectClass *klass, void *data)
     DeviceClass *dc = DEVICE_CLASS(klass);
 
     dc->realize = kvmclock_realize;
+    dc->reset = kvmclock_reset;
     dc->vmsd = &kvmclock_vmsd;
     device_class_set_props(dc, kvmclock_properties);
 }